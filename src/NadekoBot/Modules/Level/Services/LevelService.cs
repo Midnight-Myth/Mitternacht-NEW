f4817daa9053a57c5db6c0915018451ea44f3ab6
expeehaa <expeehaa@outlook.com>
2017-08-28 22:38:06 +0200

    LevelService#AddLevelRole: Fixed equality check

--- src/NadekoBot/Modules/Level/Services/LevelService.cs
+++ src/NadekoBot/Modules/Level/Services/LevelService.cs
@@ -46,12 +46,12 @@ namespace NadekoBot.Modules.Level.Services
                 sm.Channel.SendMessageAsync("1").GetAwaiter().GetResult();
                 var level = uow.LevelModel.GetLevel(user.Id);
                 var userroles = user.GetRoles().ToList();
-                sm.Channel.SendMessageAsync($"userroles count: {userroles.Count()}").GetAwaiter().GetResult();
+                sm.Channel.SendMessageAsync($"userroles count: {userroles.Count}").GetAwaiter().GetResult();
                 var rlbs = uow.RoleLevelBinding.GetAll().ToList();
                 if(!rlbs.Any()) return Task.CompletedTask;
                 sm.Channel.SendMessageAsync($"rlbs count: {rlbs.Count}").GetAwaiter().GetResult();
-                var rlb = (from rl in rlbs where rl.MinimumLevel <= level && userroles.All(r => r.Id == rl.RoleId) select rl) as List<RoleLevelBinding> ?? new List<RoleLevelBinding>();
-                sm.Channel.SendMessageAsync($"rlb count: {rlb?.Count}").GetAwaiter().GetResult();
+                var rlb = (from rl in rlbs where rl.MinimumLevel <= level && userroles.All(r => r.Id != rl.RoleId) select rl) as List<RoleLevelBinding> ?? new List<RoleLevelBinding>();
+                sm.Channel.SendMessageAsync($"rlb count: {rlb.Count}").GetAwaiter().GetResult();
                 rolesToAdd = (from r in user.Guild.Roles where rlb.Any(rs => rs.RoleId == r.Id) select r) as List<IRole> ?? new List<IRole>();
                 sm.Channel.SendMessageAsync($"rolestoadd count: {rolesToAdd.Count}").GetAwaiter().GetResult();
             }
