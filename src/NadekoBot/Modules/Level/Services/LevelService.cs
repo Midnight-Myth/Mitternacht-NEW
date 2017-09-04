1ae146c63f3fb9a0976570b26690c5f3126a02cd
expeehaa <expeehaa@outlook.com>
2017-09-04 22:57:45 +0200

    Added message xp channel blacklist check to message handlers in LevelService

--- src/NadekoBot/Modules/Level/Services/LevelService.cs
+++ src/NadekoBot/Modules/Level/Services/LevelService.cs
@@ -36,8 +36,7 @@ namespace NadekoBot.Modules.Level.Services
                     c => c.Aliases.Any(c2 => sm.Content.StartsWith(_ch.GetPrefix(user.Guild) + c2 + " ") || sm.Content.Equals(_ch.GetPrefix(user.Guild) + c2)))) return;
             
             List<IRole> rolesToAdd;
-            using (var uow = _db.UnitOfWork)
-            {
+            using (var uow = _db.UnitOfWork) {
                 var level = uow.LevelModel.GetLevel(user.Id);
                 var userroles = user.GetRoles().ToList();
                 var rlbs = uow.RoleLevelBinding.GetAll().ToList();
@@ -61,6 +60,8 @@ namespace NadekoBot.Modules.Level.Services
                 return;
 
             using (var uow = _db.UnitOfWork) {
+                if (await uow.MessageXpBlacklist.IsRestrictedAsync(sm.Channel as ITextChannel))
+                    return;
                 var time = DateTime.Now;
                 if (uow.LevelModel.CanGetMessageXp(user.Id, time)) {
                     uow.LevelModel.TryAddXp(user.Id, sm.Content.Length > 25 ? 25 : sm.Content.Length, false);
@@ -83,6 +84,8 @@ namespace NadekoBot.Modules.Level.Services
                 return;
 
             using (var uow = _db.UnitOfWork) {
+                if (await uow.MessageXpBlacklist.IsRestrictedAsync(channel as ITextChannel))
+                    return;
                 uow.LevelModel.TryAddXp(msgBefore.Author.Id, after.Content.Length - msgBefore.Content.Length, false);
                 await SendLevelChangedMessage(uow.LevelModel.CalculateLevel(after.Author.Id), after.Author, channel);
                 await uow.CompleteAsync().ConfigureAwait(false);
@@ -100,6 +103,8 @@ namespace NadekoBot.Modules.Level.Services
                     c => c.Aliases.Any(c2 => msgBefore.Content.StartsWith(_ch.GetPrefix(user.Guild) + c2 + " ") || msgBefore.Content.Equals(_ch.GetPrefix(user.Guild) + c2))))
                 return;
             using (var uow = _db.UnitOfWork) {
+                if (await uow.MessageXpBlacklist.IsRestrictedAsync(channel as ITextChannel))
+                    return;
                 uow.LevelModel.TryAddXp(msgBefore.Author.Id, msgBefore.Content.Length > 25 ? -25 : -msgBefore.Content.Length);
                 await SendLevelChangedMessage(uow.LevelModel.CalculateLevel(msgBefore.Author.Id), msgBefore.Author, channel);
                 uow.Complete();
