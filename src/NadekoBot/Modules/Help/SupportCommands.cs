a35e019371b0c35d50df62144e1f4746f18c85a1
expeehaa <expeehaa@outlook.com>
2017-09-07 18:06:15 +0200

    Added stub for .support command and .setsupportchannel

--- src/NadekoBot/Modules/Help/SupportCommands.cs
+++ src/NadekoBot/Modules/Help/SupportCommands.cs
@@ -1,4 +1,9 @@
-﻿using Discord.Commands;
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+using NadekoBot.Common.Attributes;
+using NadekoBot.Extensions;
 using NadekoBot.Modules.Help.Services;
 
 namespace NadekoBot.Modules.Help
@@ -8,7 +13,33 @@ namespace NadekoBot.Modules.Help
         [Group]
         public class SupportCommands : NadekoSubmodule<SupportService>
         {
-            
+            public SupportCommands() {
+                
+            }
+
+            [NadekoCommand, Usage, Description, Aliases]
+            [RequireContext(ContextType.Guild)]
+            public async Task Support(string text) {
+                var supportChannelId = _service.GetSupportChannelId(Context.Guild.Id);
+                if (!supportChannelId.HasValue) {
+                    await Context.Channel.SendErrorAsync("Für diesen Server ist kein Supportkanal festgelegt!");
+                    return;
+                }
+                var supportChannel = (await Context.Guild.GetTextChannelsAsync()).FirstOrDefault(t => t.Id == supportChannelId.Value);
+                if (supportChannel == null) {
+                    await Context.Channel.SendErrorAsync("Der gespeicherte Supportkanal existiert nicht. Bitte melde das einem Botowner!");
+                    return;
+                }
+                
+            }
+
+            [NadekoCommand, Usage, Description, Aliases]
+            [RequireContext(ContextType.Guild)]
+            [OwnerOnly]
+            public async Task SetSupportChannel([Remainder]ITextChannel channel = null) {
+                await _service.SetSupportChannel(Context.Guild.Id, channel?.Id);
+                await Context.Channel.SendConfirmAsync($"Supportkanal wurde {(channel == null ? "deaktiviert" : $"auf Kanal {channel.Mention} gesetzt")}.");
+            }
         }
     }
 }
