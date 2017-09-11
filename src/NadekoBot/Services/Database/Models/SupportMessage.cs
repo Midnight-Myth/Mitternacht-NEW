1a748448da286b8007c410e98dca07d138c49c2d
expeehaa <expeehaa@outlook.com>
2017-09-11 18:25:38 +0200

    Added Models, Repositories and Submodule for Support commands. WIP

---
+++ src/NadekoBot/Services/Database/Models/SupportMessage.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NadekoBot.Services.Database.Models
+{
+    public class SupportMessage : DbEntity
+    {
+        public ulong UserId { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public string Message { get; set; }
+        public bool Editor { get; set; }
+    }
+}
