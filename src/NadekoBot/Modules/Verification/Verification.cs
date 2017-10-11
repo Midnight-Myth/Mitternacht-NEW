1791bf34ac57a0b601dec6c8ee3526936f13b855
expeehaa <expeehaa@outlook.com>
2017-10-11 13:07:30 +0200

    Added Verification module commands to CommandStrings.resx

--- src/NadekoBot/Modules/Verification/Verification.cs
+++ src/NadekoBot/Modules/Verification/Verification.cs
@@ -96,6 +96,8 @@ namespace NadekoBot.Modules.Verification
                 if (role != null && Context.User is IGuildUser guildUser) await guildUser.AddRoleAsync(role);
             }
             _service.ValidationKeys.TryRemove(key);
+            var ch = await Context.User.GetOrCreateDMChannelAsync();
+            await ch.SendMessageAsync("Verification complete (WIP)");
         }
 
         [NadekoCommand, Usage, Description, Aliases]
@@ -105,7 +107,7 @@ namespace NadekoBot.Modules.Verification
             if (user == null) return;
             using (var uow = _db.UnitOfWork) {
                 var msg = uow.VerificatedUser.RemoveVerification(Context.Guild.Id, user.Id) ? await ConfirmLocalized("removed_discord", user.Username) : await ErrorLocalized("removed_discord_fail", user.Username);
-                msg.DeleteAfter(60);
+                //msg.DeleteAfter(60);
             }
         }
 
@@ -124,7 +126,7 @@ namespace NadekoBot.Modules.Verification
             var roleid = _service.GetVerifiedRoleId(Context.Guild.Id);
             if (roleid == role?.Id) return;
             await _service.SetVerifiedRole(Context.Guild.Id, role?.Id);
-            (role == null ? await ConfirmLocalized("role_not_set") : await ConfirmLocalized("role_set_not_null", role.Id)).DeleteAfter(60);
+            await (role == null ? ConfirmLocalized("role_not_set") : ConfirmLocalized("role_set_not_null", role.Name));
         }
 
         [NadekoCommand, Usage, Description, Aliases]
@@ -138,7 +140,7 @@ namespace NadekoBot.Modules.Verification
                 var role = Context.Guild.GetRole(roleid.Value);
                 msg = role == null ? await ErrorLocalized("role_not_found", roleid.Value) : await ConfirmLocalized("role", role.Name);
             }
-            msg.DeleteAfter(60);
+            //msg.DeleteAfter(60);
         }
 
         [NadekoCommand, Usage, Description, Aliases]
@@ -146,7 +148,7 @@ namespace NadekoBot.Modules.Verification
         [OwnerOnly]
         public async Task VerifyString() {
             var vs = _service.GetVerifyString(Context.Guild.Id);
-            (string.IsNullOrWhiteSpace(vs) ? await ConfirmLocalized("verifystring_void") : await ConfirmLocalized("verifystring", vs)).DeleteAfter(60);
+            await (string.IsNullOrWhiteSpace(vs) ? ConfirmLocalized("verifystring_void") : ConfirmLocalized("verifystring", vs));
         }
 
         [NadekoCommand, Usage, Description, Aliases]
@@ -156,8 +158,7 @@ namespace NadekoBot.Modules.Verification
             var old = _service.GetVerifyString(Context.Guild.Id);
             verifystring = string.Equals(verifystring, "null", StringComparison.OrdinalIgnoreCase) ? null : verifystring;
             await _service.SetVerifyString(Context.Guild.Id, verifystring);
-            var msg = await ConfirmLocalized("verifystring_new", old, verifystring);
-            msg.DeleteAfter(60);
+            await ConfirmLocalized("verifystring_new", old, verifystring);
         }
     }
 }
