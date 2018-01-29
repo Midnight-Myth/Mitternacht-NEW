e69bf74a87926b993d86a573ae109915f0b54592
expeehaa <expeehaa@outlook.com>
2018-01-29 21:39:13 +0100

    Added Birthday module, BirthDateTypeReader, birthdayset cmd stub

---
+++ src/NadekoBot/Modules/Birthday/Models/BirthDate.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Mitternacht.Modules.Birthday.Models
+{
+    public class BirthDate
+    {
+        public int Day { get; set; }
+        public int Month { get; set; }
+        public int? Year { get; set; }
+
+        public BirthDate(int day, int month, int? year = null) {
+            new DateTime(year ?? 2000, month, day);
+            Day = day;
+            Month = month;
+            Year = year;
+        }
+
+        public override string ToString()
+            => $"{Day:D2}.{Month:D2}.{(Year.HasValue ? $"{Year.Value:D4}" : string.Empty)}";
+    }
+}
