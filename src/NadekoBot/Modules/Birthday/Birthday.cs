6fbafd2573c0d4fab9cc22de47bc3d25b3213451
expeehaa <expeehaa@outlook.com>
2018-02-28 21:12:46 +0100

    Slightly modified birthdaysall cmd

--- src/NadekoBot/Modules/Birthday/Birthday.cs
+++ src/NadekoBot/Modules/Birthday/Birthday.cs
@@ -19,17 +19,21 @@ namespace Mitternacht.Modules.Birthday
         private readonly DbService _db;
         private readonly IBotCredentials _bc;
 
-        public Birthday(DbService db, IBotCredentials bc) {
+        public Birthday(DbService db, IBotCredentials bc)
+        {
             _db = db;
             _bc = bc;
         }
 
         [MitternachtCommand, Usage, Description, Aliases]
         [RequireContext(ContextType.Guild)]
-        public async Task BirthdaySet(IBirthDate bd) {
-            using (var uow = _db.UnitOfWork) {
+        public async Task BirthdaySet(IBirthDate bd)
+        {
+            using (var uow = _db.UnitOfWork)
+            {
                 var bdm = uow.BirthDates.GetUserBirthDate(Context.User.Id);
-                if (!_bc.IsOwner(Context.User) && bdm?.Year != null || bdm != null && bdm.Year == null && (bdm.Day != bd.Day || bdm.Month != bd.Month)) {
+                if (!_bc.IsOwner(Context.User) && bdm?.Year != null || bdm != null && bdm.Year == null && (bdm.Day != bd.Day || bdm.Month != bd.Month))
+                {
                     await ReplyErrorLocalized("set_before").ConfigureAwait(false);
                     return;
                 }
@@ -58,8 +62,10 @@ namespace Mitternacht.Modules.Birthday
 
         [MitternachtCommand, Usage, Description, Aliases]
         [OwnerOnly]
-        public async Task BirthdayRemove(IUser user) {
-            using (var uow = _db.UnitOfWork) {
+        public async Task BirthdayRemove(IUser user)
+        {
+            using (var uow = _db.UnitOfWork)
+            {
                 var success = uow.BirthDates.DeleteBirthDate(user.Id);
                 if (success)
                     await ConfirmLocalized("removed", user.ToString()).ConfigureAwait(false);
@@ -71,11 +77,13 @@ namespace Mitternacht.Modules.Birthday
 
         [MitternachtCommand, Usage, Description, Aliases]
         [RequireContext(ContextType.Guild)]
-        public async Task BirthdayGet(IUser user = null) {
+        public async Task BirthdayGet(IUser user = null)
+        {
             user = user ?? Context.User;
-            using (var uow = _db.UnitOfWork) {
+            using (var uow = _db.UnitOfWork)
+            {
                 var bdm = uow.BirthDates.GetUserBirthDate(user.Id);
-                var age = bdm?.Year != null ? (int) Math.Floor((DateTime.Now - new DateTime(bdm.Year.Value, bdm.Month, bdm.Day)).TotalDays / 365.25) : 0;
+                var age = bdm?.Year != null ? (int)Math.Floor((DateTime.Now - new DateTime(bdm.Year.Value, bdm.Month, bdm.Day)).TotalDays / 365.25) : 0;
                 if (user == Context.User)
                     if (bdm == null)
                         await ReplyErrorLocalized("self_none").ConfigureAwait(false);
@@ -85,7 +93,7 @@ namespace Mitternacht.Modules.Birthday
                         await ReplyConfirmLocalized("self_age", bdm.ToString(), age).ConfigureAwait(false);
                 else if (bdm == null)
                     await ReplyErrorLocalized("user_none", user.ToString()).ConfigureAwait(false);
-                else if(bdm.Year == null)
+                else if (bdm.Year == null)
                     await ReplyConfirmLocalized("user", user.ToString(), bdm.ToString()).ConfigureAwait(false);
                 else
                     await ReplyConfirmLocalized("user_age", user.ToString(), bdm.ToString(), age).ConfigureAwait(false);
@@ -94,14 +102,17 @@ namespace Mitternacht.Modules.Birthday
 
         [MitternachtCommand, Usage, Description, Aliases]
         [RequireContext(ContextType.Guild)]
-        public async Task Birthdays(IBirthDate bd = null) {
+        public async Task Birthdays(IBirthDate bd = null)
+        {
             bd = bd ?? BirthDate.Today;
             List<BirthDateModel> birthdates;
-            using (var uow = _db.UnitOfWork) {
+            using (var uow = _db.UnitOfWork)
+            {
                 birthdates = uow.BirthDates.GetBirthdays(bd, true).ToList();
             }
 
-            if (!birthdates.Any()) {
+            if (!birthdates.Any())
+            {
                 await ConfirmLocalized("none_date", bd.ToString()).ConfigureAwait(false);
                 return;
             }
@@ -110,21 +121,24 @@ namespace Mitternacht.Modules.Birthday
                 .WithOkColor()
                 .WithTitle(GetText("list_title", bd.ToString()))
                 .WithDescription(string.Join("\n", from bdm in birthdates
-                                 select $"- {Context.Client.GetUserAsync(bdm.UserId).GetAwaiter().GetResult()?.ToString() ?? bdm.UserId.ToString()}{(bdm.Year.HasValue && !bd.Year.HasValue ? $"{BirthDate.Today.Year - bdm.Year}" : "")}"));
+                                                   select $"- {Context.Client.GetUserAsync(bdm.UserId).GetAwaiter().GetResult()?.ToString() ?? bdm.UserId.ToString()}{(bdm.Year.HasValue && !bd.Year.HasValue ? $"{BirthDate.Today.Year - bdm.Year}" : "")}"));
             await Context.Channel.EmbedAsync(eb).ConfigureAwait(false);
         }
 
         [MitternachtCommand, Usage, Description, Aliases]
         [RequireContext(ContextType.Guild)]
-        public async Task BirthdaysAll(int page = 1) {
+        public async Task BirthdaysAll(int page = 1)
+        {
             if (page < 1) page = 1;
 
             List<BirthDateModel> birthdates;
-            using (var uow = _db.UnitOfWork) {
+            using (var uow = _db.UnitOfWork)
+            {
                 birthdates = uow.BirthDates.GetAll().OrderBy(bdm => bdm.Month).ThenBy(bdm => bdm.Day).ToList();
             }
 
-            if (!birthdates.Any()) {
+            if (!birthdates.Any())
+            {
                 await ErrorLocalized("none").ConfigureAwait(false);
                 return;
             }
@@ -133,12 +147,13 @@ namespace Mitternacht.Modules.Birthday
             var pagecount = (int)Math.Ceiling(birthdates.Count / (itemcount * 1d));
             if (page > pagecount) page = pagecount;
 
-            await Context.Channel.SendPaginatedConfirmAsync(Context.Client as DiscordSocketClient, page - 1, p => new EmbedBuilder()
-                        .WithOkColor()
-                        .WithTitle(GetText("all_title"))
-                        .WithDescription(string.Join("\n", from bdm in birthdates.Skip(itemcount * p).Take(itemcount)
-                            select $"- {Context.Client.GetUserAsync(bdm.UserId).GetAwaiter().GetResult()?.ToString() ?? bdm.UserId.ToString()} - **{bdm.ToString()}**")), pagecount - 1, reactUsers: new[] {Context.User as IGuildUser})
-                .ConfigureAwait(false);
+            await Context.Channel.SendPaginatedConfirmAsync(Context.Client as DiscordSocketClient, page - 1,
+                p => new EmbedBuilder().WithOkColor().WithTitle(GetText("all_title")).WithDescription(string.Join("\n",
+                    birthdates.Skip(itemcount * p).Take(itemcount).Select(BdmToString))),
+                pagecount - 1, reactUsers: new[] { Context.User as IGuildUser }).ConfigureAwait(false);
         }
+
+        private string BdmToString(BirthDateModel bdm)
+            => $"- {Context.Client.GetUserAsync(bdm.UserId).GetAwaiter().GetResult()?.ToString() ?? bdm.UserId.ToString()} - **{bdm}**";
     }
 }
