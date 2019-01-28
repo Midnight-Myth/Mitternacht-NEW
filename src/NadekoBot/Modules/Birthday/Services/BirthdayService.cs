570deeec9a514059a79dda835a3e7157da428398
expeehaa <expeehaa@outlook.com>
2019-01-29 00:59:14 +0100

    Rewrite BirthdayService with events.

--- src/NadekoBot/Modules/Birthday/Services/BirthdayService.cs
+++ src/NadekoBot/Modules/Birthday/Services/BirthdayService.cs
@@ -1,12 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
 using System.Threading.Tasks;
 using Discord.WebSocket;
 using Mitternacht.Extensions;
 using Mitternacht.Services;
-using Mitternacht.Services.Database.Models;
 using NLog;
 
 
@@ -19,6 +17,9 @@ namespace Mitternacht.Modules.Birthday.Services
         private readonly Task _timerTask;
 
         private const int BirthdayCheckRepeatDelay = 60 * 1000;
+
+        public event Func<SocketGuildUser[], Task> UserBirthdayStarting = u => Task.CompletedTask;
+        public event Func<SocketGuildUser[], Task> BirthdayUsers = u => Task.CompletedTask;
 
         public BirthdayService(DbService db, DiscordSocketClient client)
         {
@@ -41,65 +42,17 @@ namespace Mitternacht.Modules.Birthday.Services
                     await Task.Delay(BirthdayCheckRepeatDelay);
                 }
             });
-        }
 
-        private async Task OnUsersBirthday(IReadOnlyCollection<BirthDateModel> birthdays, bool newDay)
-        {
-            using (var uow = _db.UnitOfWork)
-            {
-                //_log.Info($"{birthdays.Count()} birthdays today");
-
-                var guildBirthdayRoles = uow.GuildConfigs
-                    .GetAllGuildConfigs(_client.Guilds.Select(g => g.Id).ToList())
-                    .Where(gc => gc.BirthdaysEnabled)
-                    .Select(gc => (Guild: _client.GetGuild(gc.GuildId), gc.BirthdayRoleId, MessageChannelId: gc.BirthdayMessageChannelId, Message: gc.BirthdayMessage))
-                    .ToList();
-
-                //maybe running in parallel?
-                foreach (var (guild, birthdayRoleId, msgChannelId, msg) in guildBirthdayRoles)
-                {
-                    var birthdayusers = birthdays.Select(b => guild.GetUser(b.UserId)).Where(u => u != null).ToList();
-
-                    //birthday role handling
-                    if (birthdayRoleId.HasValue)
-                    {
-                        var br = guild.GetRole(birthdayRoleId.Value);
-                        if (br != null)
-                        {
-                            var oldBirthdayRoleMembers = (await br.GetMembersAsync().ConfigureAwait(false)).ToList();
-                            //Remove birthday role from all who currently have it
-                            foreach (var gu in oldBirthdayRoleMembers.Where(u => birthdayusers.All(gu => gu.Id != u.Id)).ToList())
-                            {
-                                await gu.RemoveRoleAsync(br).ConfigureAwait(false);
-                            }
-
-                            //add birthday role to everyone having birthday
-                            foreach (var gu in birthdayusers.Where(gu => oldBirthdayRoleMembers.All(u => u.Id != gu.Id)).ToList())
-                            {
-                                await gu.AddRoleAsync(br).ConfigureAwait(false);
-                            }
-                        }
-                    }
-
-                    //birthday message handling
-                    if (newDay && msgChannelId.HasValue)
-                    {
-                        var ch = guild.GetTextChannel(msgChannelId.Value);
-                        if (birthdayusers.Any() && ch != null)
-                        {
-                            await ch.SendMessageAsync(string.Format(msg, string.Join(", ", birthdayusers.Select(u => u.Mention).ToList()))).ConfigureAwait(false);
-                        }
-                    }
-                }
-            }
+            UserBirthdayStarting += OnUserBirthdayStarting;
+            BirthdayUsers += OnBirthdayUsers;
         }
 
         private async Task TimerHandler()
         {
             using (var uow = _db.UnitOfWork)
             {
-                var time = DateTime.Now;
-                //var time = CustomTimeForTesting;
+                //var time = DateTime.Now;
+                var time = CustomTimeForTesting;
                 var birthdays = uow.BirthDates.GetBirthdays(time).ToList();
                 var bc = uow.BotConfig.GetOrCreate();
                 var newDay = bc.LastTimeBirthdaysChecked.IsOtherDate(time);
@@ -107,26 +60,87 @@ namespace Mitternacht.Modules.Birthday.Services
                 uow.BotConfig.Update(bc);
                 await uow.CompleteAsync().ConfigureAwait(false);
 
-                await OnUsersBirthday(birthdays, newDay).ConfigureAwait(false);
+                //event logic
+                var guildConfigs = uow.GuildConfigs.GetAllGuildConfigs(_client.Guilds.Select(g => g.Id).ToList()).Where(gc => gc.BirthdaysEnabled).ToList();
+                var birthdayUsers = guildConfigs.SelectMany(gc => birthdays.Select(b => _client.GetGuild(gc.GuildId).GetUser(b.UserId)).Where(u => u != null).ToList()).ToArray();
+
+                await BirthdayUsers.Invoke(birthdayUsers).ConfigureAwait(false);
+                if (newDay && birthdayUsers.Any())
+                    await UserBirthdayStarting.Invoke(birthdayUsers).ConfigureAwait(false);
             }
         }
 
-        //private int _customTimeAccessCount = 0;
-        //private DateTime CustomTimeForTesting
-        //{
-        //    get
-        //    {
-        //        using (var uow = _db.UnitOfWork)
-        //        {
-        //            var bc = uow.BotConfig.GetOrCreate();
-        //            bc.LastTimeBirthdaysChecked = _customTimeAccessCount % 2 == 0 ? DateTime.Today : DateTime.MinValue;
-        //            var time = _customTimeAccessCount % 2 == 0 ? DateTime.Today.AddDays(2) : DateTime.Today;
-        //            uow.BotConfig.Update(bc);
-        //            uow.Complete();
-        //            _customTimeAccessCount++;
-        //            return time;
-        //        }
-        //    }
-        //}
+        private async Task OnUserBirthdayStarting(SocketGuildUser[] birthdayUsers)
+        {
+            using(var uow = _db.UnitOfWork)
+            {
+                var usersGuildGroups = birthdayUsers.GroupBy(u => u.Guild).ToList();
+
+                foreach (var group in usersGuildGroups)
+                {
+                    var guild = group.Key;
+                    var gc = uow.GuildConfigs.For(guild.Id);
+
+                    var msgChannelId = gc.BirthdayMessageChannelId;
+                    if (!msgChannelId.HasValue) continue;
+                    var ch = guild.GetTextChannel(msgChannelId.Value);
+
+                    var msg = gc.BirthdayMessage;
+
+                    if (ch != null)
+                        await ch.SendMessageAsync(string.Format(msg, string.Join(", ", group.Select(u => u.Mention).ToList()))).ConfigureAwait(false);
+                }
+            }
+        }
+
+        private async Task OnBirthdayUsers(SocketGuildUser[] birthdayUsers)
+        {
+            using(var uow = _db.UnitOfWork)
+            {
+                var guildConfigs = uow.GuildConfigs.GetAllGuildConfigs(_client.Guilds.Select(g => g.Id).ToList()).Where(gc => gc.BirthdaysEnabled).ToList();
+
+                foreach (var gc in guildConfigs)
+                {
+                    var guild = _client.GetGuild(gc.GuildId);
+                    var birthdayRoleId = gc.BirthdayRoleId;
+                    if (!birthdayRoleId.HasValue) continue;
+
+                    var birthdayRole = guild.GetRole(birthdayRoleId.Value);
+                    if (birthdayRole == null) continue;
+
+                    var oldBirthdayRoleMembers = (await birthdayRole.GetMembersAsync().ConfigureAwait(false)).ToList();
+                    var guildBirthdayUsers = birthdayUsers.Where(bu => bu.Guild.Id == gc.GuildId).ToList();
+
+                    //Remove birthday role from all users who currently have it
+                    foreach (var guildUser in oldBirthdayRoleMembers.Where(u => guildBirthdayUsers.All(gu => gu.Id != u.Id)).ToList())
+                    {
+                        await guildUser.RemoveRoleAsync(birthdayRole).ConfigureAwait(false);
+                    }
+
+                    //add birthday role to everyone having birthday
+                    foreach (var guildUser in guildBirthdayUsers.Where(bu => oldBirthdayRoleMembers.All(obrm => obrm.Id != bu.Id)).ToList())
+                    {
+                        await guildUser.AddRoleAsync(birthdayRole).ConfigureAwait(false);
+                    }
+                }
+            }
+        }
+
+        private int _customTimeAccessCount = 0;
+        private DateTime CustomTimeForTesting
+        {
+            get {
+                using (var uow = _db.UnitOfWork)
+                {
+                    var bc = uow.BotConfig.GetOrCreate();
+                    bc.LastTimeBirthdaysChecked = _customTimeAccessCount % 2 == 0 ? DateTime.Today : DateTime.MinValue;
+                    var time = _customTimeAccessCount % 2 == 0 ? DateTime.Today.AddDays(2) : DateTime.Today;
+                    uow.BotConfig.Update(bc);
+                    uow.Complete();
+                    _customTimeAccessCount++;
+                    return time;
+                }
+            }
+        }
     }
 }
