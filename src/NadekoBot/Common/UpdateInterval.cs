8dc91f6448510cd4c71e7e8961fb6eb6822ea1ad
expeehaa <expeehaa@outlook.com>
2019-02-09 19:55:14 +0100

    Modify UpdateInterval constants.

--- src/NadekoBot/Common/UpdateInterval.cs
+++ src/NadekoBot/Common/UpdateInterval.cs
@@ -3,7 +3,7 @@
     public class UpdateInterval
     {
         public const int WaitForForum = 500;
-        public const int TeamUpdate = 1 * 60 * 1000;
-        public const int Birthday = 1* 60 * 1000;
+        public const int TeamUpdate = 60 * 1000;
+        public const int Birthday = 60 * 1000;
     }
 }
