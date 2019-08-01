752f4c52293986063155043d71c263c45a9ce2e6
expeehaa <expeehaa@outlook.com>
2019-08-01 03:03:17 +0200

    Add missing migration.

---
+++ src/NadekoBot/Migrations/20190801010255_VerificationPasswordChannelId.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Mitternacht.Migrations
+{
+    public partial class VerificationPasswordChannelId : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<ulong>(
+                name: "VerificationPasswordChannelId",
+                table: "GuildConfigs",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "VerificationPasswordChannelId",
+                table: "GuildConfigs");
+        }
+    }
+}
