bc66636e9476735b8daeb2c4856d5306e738ebed
expeehaa <expeehaa@outlook.com>
2019-09-28 21:56:46 +0200

    Format file, add usernamehistory commands overloads for userIds.

--- src/NadekoBot/Modules/Utility/UsernameHistoryCommands.cs
+++ src/NadekoBot/Modules/Utility/UsernameHistoryCommands.cs
@@ -11,163 +11,165 @@ using Mitternacht.Modules.Utility.Services;
 using Mitternacht.Services;
 using Mitternacht.Services.Database.Models;
 
-namespace Mitternacht.Modules.Utility
-{
-    public partial class Utility
-    {
-        [Group]
-        public class UsernameHistoryCommands : MitternachtSubmodule<UsernameHistoryService>
-        {
-            private readonly DbService _db;
+namespace Mitternacht.Modules.Utility {
+	public partial class Utility {
+		[Group]
+		public class UsernameHistoryCommands : MitternachtSubmodule<UsernameHistoryService> {
+			private readonly DbService _db;
 
-            public UsernameHistoryCommands(DbService db) {
-                _db = db;
-            }
+			public UsernameHistoryCommands(DbService db) {
+				_db = db;
+			}
 
-            [MitternachtCommand, Description, Usage, Aliases]
-            [OwnerOnly]
-            public async Task ToggleUsernameHistory() {
-                bool logging;
-                using (var uow = _db.UnitOfWork) {
-                    var bc = uow.BotConfig.GetOrCreate(set => set);
-                    logging = bc.LogUsernames = !bc.LogUsernames;
-                    uow.BotConfig.Update(bc);
-                    await uow.CompleteAsync().ConfigureAwait(false);
-                }
+			[MitternachtCommand, Description, Usage, Aliases]
+			[OwnerOnly]
+			public async Task ToggleUsernameHistory() {
+				using(var uow = _db.UnitOfWork) {
+					var bc      = uow.BotConfig.GetOrCreate(set => set);
+					var logging = bc.LogUsernames = !bc.LogUsernames;
+					uow.BotConfig.Update(bc);
+					await uow.CompleteAsync().ConfigureAwait(false);
 
-                await ConfirmLocalized("unh_log_global", GetActiveText(logging)).ConfigureAwait(false);
-            }
+					await ConfirmLocalized("unh_log_global", GetActiveText(logging)).ConfigureAwait(false);
+				}
+			}
 
-            [MitternachtCommand, Description, Usage, Aliases]
-            [OwnerOnly]
-            public async Task ToggleUsernameHistoryGuild(bool? toggle = null, IGuild guild = null) {
-                guild = guild ?? Context.Guild;
-                if (guild == null)
-                {
-                    await ErrorLocalized("unh_guild_null").ConfigureAwait(false);
-                    return;
-                }
+			[MitternachtCommand, Description, Usage, Aliases]
+			[OwnerOnly]
+			public async Task ToggleUsernameHistoryGuild(bool? toggle = null, IGuild guild = null) {
+				guild = guild ?? Context.Guild;
+				if(guild == null) {
+					await ErrorLocalized("unh_guild_null").ConfigureAwait(false);
+					return;
+				}
 
-                bool? loggingBefore;
-                bool globalLogging;
-                using (var uow = _db.UnitOfWork) {
-                    globalLogging = uow.BotConfig.GetOrCreate(set => set).LogUsernames;
-                    var gc = uow.GuildConfigs.For(guild.Id, set => set);
-                    loggingBefore = gc.LogUsernameHistory;
-                    if (loggingBefore == toggle)
-                    {
-                        await ErrorLocalized("unh_guild_log_equals", guild.Name, GetActiveText(toggle)).ConfigureAwait(false);
-                        return;
-                    }
-                    gc.LogUsernameHistory = toggle;
-                    await uow.CompleteAsync().ConfigureAwait(false);
-                }
+				using(var uow = _db.UnitOfWork) {
+					var globalLogging = uow.BotConfig.GetOrCreate(set => set).LogUsernames;
+					var gc            = uow.GuildConfigs.For(guild.Id, set => set);
+					var loggingBefore = gc.LogUsernameHistory;
+					if(loggingBefore == toggle) {
+						await ErrorLocalized("unh_guild_log_equals", guild.Name, GetActiveText(toggle)).ConfigureAwait(false);
+						return;
+					}
 
-                await Context.Channel.SendConfirmAsync(GetText("unh_log_guild", guild.Name, GetActiveText(loggingBefore), GetActiveText(toggle)).Trim() + " " + GetText("unh_log_global_append", GetActiveText(globalLogging)).Trim()).ConfigureAwait(false);
-            }
+					gc.LogUsernameHistory = toggle;
+					await uow.CompleteAsync().ConfigureAwait(false);
 
-            [MitternachtCommand, Description, Usage, Aliases]
-            [RequireContext(ContextType.Guild)]
-            public async Task UsernameHistory(IUser user = null, int page = 1) {
-                user = user ?? Context.User;
-                List<UsernameHistoryModel> usernicknames;
-                using (var uow = _db.UnitOfWork) {
-                    var nicknames = uow.NicknameHistory.GetGuildUserNames(Context.Guild.Id, user.Id);
-                    var usernames = uow.UsernameHistory.GetUserNames(user.Id);
-                    usernicknames = usernames.Concat(nicknames).OrderByDescending(u => u.DateSet).ToList();
-                }
+					await Context.Channel.SendConfirmAsync(GetText("unh_log_guild", guild.Name, GetActiveText(loggingBefore), GetActiveText(toggle)).Trim() + " " + GetText("unh_log_global_append", GetActiveText(globalLogging)).Trim()).ConfigureAwait(false);
+				}
+			}
 
-                if (!usernicknames.Any()) {
-                    await ErrorLocalized("unh_no_names", user.ToString()).ConfigureAwait(false);
-                    return;
-                }
-                if (page < 1) page = 1;
+			[MitternachtCommand, Description, Usage, Aliases]
+			[RequireContext(ContextType.Guild)]
+			public async Task UsernameHistory(IUser user = null, int page = 1) {
+				user = user ?? Context.User;
+				await UsernameHistory(user.Id, page);
+			}
 
-                const int elementsPerPage = 10;
-                var pagecount = (int)Math.Ceiling(usernicknames.Count / (elementsPerPage * 1d));
-                if (page > pagecount) page = pagecount;
-                await Context.Channel.SendPaginatedConfirmAsync(Context.Client as DiscordSocketClient, page - 1, p => {
-                        var embed = new EmbedBuilder()
-                            .WithOkColor()
-                            .WithTitle(GetText("unh_title", user.ToString()))
-                            .WithDescription(string.Join("\n",
-                                usernicknames.Skip(p * elementsPerPage).Take(elementsPerPage).Select(uhm =>
-                                    $"- `{uhm.Name}#{uhm.DiscordDiscriminator:D4}`{(uhm is NicknameHistoryModel ? "" : " **(G)**")} - {uhm.DateSet.ToLocalTime():dd.MM.yyyy HH:mm}{(uhm.DateReplaced.HasValue ? $" => {uhm.DateReplaced.Value.ToLocalTime():dd.MM.yyyy HH:mm}" : "")}")));
-                        return embed;
-                    }, pagecount - 1, reactUsers: new []{Context.User as IGuildUser}, hasPerms: gp => gp.KickMembers).ConfigureAwait(false);
-            }
+			[MitternachtCommand, Description, Usage, Aliases]
+			[RequireContext(ContextType.Guild)]
+			public async Task UsernameHistory(ulong userId, int page = 1) {
+				var username = (await Context.Guild.GetUserAsync(userId))?.ToString() ?? userId.ToString();
 
-            [MitternachtCommand, Description, Usage, Aliases]
-            public async Task UsernameHistoryGlobal(IUser user = null, int page = 1) {
-                user = user ?? Context.User;
-                List<UsernameHistoryModel> usernames;
-                using (var uow = _db.UnitOfWork)
-                {
-                    usernames = uow.UsernameHistory.GetUserNames(user.Id).OrderByDescending(u => u.DateSet).ToList();
-                }
+				List<UsernameHistoryModel> usernicknames;
+				using(var uow = _db.UnitOfWork) {
+					var nicknames = uow.NicknameHistory.GetGuildUserNames(Context.Guild.Id, userId);
+					var usernames = uow.UsernameHistory.GetUserNames(userId);
+					usernicknames = usernames.Concat(nicknames).OrderByDescending(u => u.DateSet).ToList();
+				}
 
-                if (!usernames.Any())
-                {
-                    await ErrorLocalized("unh_no_names", user.ToString()).ConfigureAwait(false);
-                    return;
-                }
-                if (page < 1) page = 1;
+				if(!usernicknames.Any()) {
+					await ErrorLocalized("unh_no_names", username).ConfigureAwait(false);
+					return;
+				}
 
-                const int elementsPerPage = 10;
-                var pagecount = (int)Math.Ceiling(usernames.Count / (elementsPerPage * 1d));
-                if (page > pagecount) page = pagecount;
-                await Context.Channel.SendPaginatedConfirmAsync(Context.Client as DiscordSocketClient, page - 1, p => {
-                        var embed = new EmbedBuilder()
-                            .WithOkColor()
-                            .WithTitle(GetText("unh_title_global", user.ToString()))
-                            .WithDescription(string.Join("\n",
-                                usernames.Skip(p * elementsPerPage).Take(elementsPerPage).Select(uhm =>
-                                    $"- `{uhm.Name}#{uhm.DiscordDiscriminator:D4}` - {uhm.DateSet.ToLocalTime():dd.MM.yyyy HH:mm}{(uhm.DateReplaced.HasValue ? $" => {uhm.DateReplaced.Value.ToLocalTime():dd.MM.yyyy HH:mm}" : "")}")));
-                        return embed;
-                    }, pagecount - 1, reactUsers: new[] { Context.User as IGuildUser }, hasPerms: gp => gp.KickMembers).ConfigureAwait(false);
-            }
+				if(page < 1) page = 1;
 
-            [MitternachtCommand, Description, Usage, Aliases]
-            [RequireContext(ContextType.Guild)]
-            public async Task UsernameHistoryGuild(IUser user = null, int page = 1)
-            {
-                user = user ?? Context.User;
-                List<NicknameHistoryModel> nicknames;
-                using (var uow = _db.UnitOfWork)
-                {
-                    nicknames = uow.NicknameHistory.GetGuildUserNames(Context.Guild.Id, user.Id).OrderByDescending(u => u.DateSet).ToList();
-                }
+				const int elementsPerPage = 10;
+				var       pagecount       = (int)Math.Ceiling(usernicknames.Count / (elementsPerPage * 1d));
+				if(page > pagecount) page = pagecount;
+				await Context.Channel.SendPaginatedConfirmAsync(Context.Client as DiscordSocketClient, page - 1, p => {
+																	var embed = new EmbedBuilder().WithOkColor().WithTitle(GetText("unh_title", username)).WithDescription(string.Join("\n", usernicknames.Skip(p * elementsPerPage).Take(elementsPerPage).Select(uhm => $"- `{uhm.Name}#{uhm.DiscordDiscriminator:D4}`{(uhm is NicknameHistoryModel ? "" : " **(G)**")} - {uhm.DateSet.ToLocalTime():dd.MM.yyyy HH:mm}{(uhm.DateReplaced.HasValue ? $" => {uhm.DateReplaced.Value.ToLocalTime():dd.MM.yyyy HH:mm}" : "")}")));
+																	return embed;
+																}, pagecount - 1, reactUsers: new[] {Context.User as IGuildUser}, hasPerms: gp => gp.KickMembers)
+							.ConfigureAwait(false);
+			}
 
-                if (!nicknames.Any())
-                {
-                    await ErrorLocalized("unh_no_names", user.ToString()).ConfigureAwait(false);
-                    return;
-                }
-                if (page < 1) page = 1;
+			[MitternachtCommand, Description, Usage, Aliases]
+			public async Task UsernameHistoryGlobal(IUser user = null, int page = 1) {
+				user = user ?? Context.User;
+				await UsernameHistoryGlobal(user.Id, page);
+			}
 
-                const int elementsPerPage = 10;
-                var pagecount = (int)Math.Ceiling(nicknames.Count / (elementsPerPage * 1d));
-                if (page > pagecount) page = pagecount;
-                await Context.Channel.SendPaginatedConfirmAsync(Context.Client as DiscordSocketClient, page - 1, p => {
-                        var embed = new EmbedBuilder()
-                            .WithOkColor()
-                            .WithTitle(GetText("unh_title_guild", user.ToString()))
-                            .WithDescription(string.Join("\n",
-                                nicknames.Skip(p * elementsPerPage).Take(elementsPerPage).Select(uhm =>
-                                    $"- `{uhm.Name}#{uhm.DiscordDiscriminator:D4}` - {uhm.DateSet.ToLocalTime():dd.MM.yyyy HH:mm}{(uhm.DateReplaced.HasValue ? $" => {uhm.DateReplaced.Value.ToLocalTime():dd.MM.yyyy HH:mm}" : "")}")));
-                        return embed;
-                    }, pagecount - 1, reactUsers: new[] { Context.User as IGuildUser }, hasPerms: gp => gp.KickMembers).ConfigureAwait(false);
-            }
+			[MitternachtCommand, Description, Usage, Aliases]
+			public async Task UsernameHistoryGlobal(ulong userId, int page = 1) {
+				var username = (await Context.Client.GetUserAsync(userId))?.ToString() ?? userId.ToString();
 
-            private string GetActiveText(bool? setting)
-                => GetText(setting.HasValue ? setting.Value ? "unh_active" : "unh_inactive" : "unh_global");
+				List<UsernameHistoryModel> usernames;
+				using(var uow = _db.UnitOfWork) {
+					usernames = uow.UsernameHistory.GetUserNames(userId).OrderByDescending(u => u.DateSet).ToList();
+				}
 
-            [MitternachtCommand, Description, Aliases, Usage]
-            [OwnerOnly]
-            public async Task UpdateUsernames() {
-                var (nicks, usernames, users, time) = await Service.UpdateUsernames().ConfigureAwait(false);
-                await ConfirmLocalized("unh_update_usernames", nicks, usernames, users, $"{time.TotalSeconds:F2}s").ConfigureAwait(false);
-            }
-        }
-    }
+				if(!usernames.Any()) {
+					await ErrorLocalized("unh_no_names", username).ConfigureAwait(false);
+					return;
+				}
+
+				if(page < 1) page = 1;
+
+				const int elementsPerPage = 10;
+				var       pagecount       = (int)Math.Ceiling(usernames.Count / (elementsPerPage * 1d));
+				if(page > pagecount) page = pagecount;
+				await Context.Channel.SendPaginatedConfirmAsync(Context.Client as DiscordSocketClient, page - 1, p => {
+																	var embed = new EmbedBuilder().WithOkColor().WithTitle(GetText("unh_title_global", username)).WithDescription(string.Join("\n", usernames.Skip(p * elementsPerPage).Take(elementsPerPage).Select(uhm => $"- `{uhm.Name}#{uhm.DiscordDiscriminator:D4}` - {uhm.DateSet.ToLocalTime():dd.MM.yyyy HH:mm}{(uhm.DateReplaced.HasValue ? $" => {uhm.DateReplaced.Value.ToLocalTime():dd.MM.yyyy HH:mm}" : "")}")));
+																	return embed;
+																}, pagecount - 1, reactUsers: new[] {Context.User as IGuildUser}, hasPerms: gp => gp.KickMembers)
+							.ConfigureAwait(false);
+			}
+
+			[MitternachtCommand, Description, Usage, Aliases]
+			[RequireContext(ContextType.Guild)]
+			public async Task UsernameHistoryGuild(IUser user = null, int page = 1) {
+				user = user ?? Context.User;
+				await UsernameHistoryGuild(user.Id, page);
+			}
+
+			[MitternachtCommand, Description, Usage, Aliases]
+			[RequireContext(ContextType.Guild)]
+			public async Task UsernameHistoryGuild(ulong userId, int page = 1) {
+				var username = (await Context.Guild.GetUserAsync(userId))?.ToString() ?? userId.ToString();
+
+				List<NicknameHistoryModel> nicknames;
+				using(var uow = _db.UnitOfWork) {
+					nicknames = uow.NicknameHistory.GetGuildUserNames(Context.Guild.Id, userId).OrderByDescending(u => u.DateSet).ToList();
+				}
+
+				if(!nicknames.Any()) {
+					await ErrorLocalized("unh_no_names", username).ConfigureAwait(false);
+					return;
+				}
+
+				if(page < 1) page = 1;
+
+				const int elementsPerPage = 10;
+				var       pagecount       = (int)Math.Ceiling(nicknames.Count / (elementsPerPage * 1d));
+				if(page > pagecount) page = pagecount;
+				await Context.Channel.SendPaginatedConfirmAsync(Context.Client as DiscordSocketClient, page - 1, p => {
+																	var embed = new EmbedBuilder().WithOkColor().WithTitle(GetText("unh_title_guild", username)).WithDescription(string.Join("\n", nicknames.Skip(p * elementsPerPage).Take(elementsPerPage).Select(uhm => $"- `{uhm.Name}#{uhm.DiscordDiscriminator:D4}` - {uhm.DateSet.ToLocalTime():dd.MM.yyyy HH:mm}{(uhm.DateReplaced.HasValue ? $" => {uhm.DateReplaced.Value.ToLocalTime():dd.MM.yyyy HH:mm}" : "")}")));
+																	return embed;
+																}, pagecount - 1, reactUsers: new[] {Context.User as IGuildUser}, hasPerms: gp => gp.KickMembers)
+							.ConfigureAwait(false);
+			}
+
+			private string GetActiveText(bool? setting)
+				=> GetText(setting.HasValue ? setting.Value ? "unh_active" : "unh_inactive" : "unh_global");
+
+			[MitternachtCommand, Description, Aliases, Usage]
+			[OwnerOnly]
+			public async Task UpdateUsernames() {
+				var (nicks, usernames, users, time) = await Service.UpdateUsernames().ConfigureAwait(false);
+				await ConfirmLocalized("unh_update_usernames", nicks, usernames, users, $"{time.TotalSeconds:F2}s").ConfigureAwait(false);
+			}
+		}
+	}
 }
