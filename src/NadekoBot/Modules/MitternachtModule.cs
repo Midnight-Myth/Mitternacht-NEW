ccaf64f0be800e9282d0862a6703d0c6ec25af39
expeehaa <expeehaa@outlook.com>
2019-09-29 18:08:02 +0200

    Make Reply methods protected, format file.

--- src/NadekoBot/Modules/MitternachtModule.cs
+++ src/NadekoBot/Modules/MitternachtModule.cs
@@ -8,156 +8,111 @@ using Mitternacht.Services;
 using Mitternacht.Services.Impl;
 using NLog;
 
-namespace Mitternacht.Modules
-{
-    public abstract class MitternachtTopLevelModule : ModuleBase
-    {
-        protected readonly Logger _log;
-        protected CultureInfo CultureInfo;
+namespace Mitternacht.Modules {
+	public abstract class MitternachtTopLevelModule : ModuleBase {
+		protected readonly Logger      _log;
+		protected          CultureInfo CultureInfo;
 
-        public readonly string ModuleTypeName;
-        public string LowerModuleTypeName => ModuleTypeName?.ToLowerInvariant();
+		public readonly string ModuleTypeName;
+		public          string LowerModuleTypeName => ModuleTypeName?.ToLowerInvariant();
 
-        public StringService Strings { get; set; }
-        public CommandHandler CmdHandler { get; set; }
-        public ILocalization Localization { get; set; }
+		public StringService  Strings      { get; set; }
+		public CommandHandler CmdHandler   { get; set; }
+		public ILocalization  Localization { get; set; }
 
-        public string Prefix => CmdHandler.GetPrefix(Context.Guild);
+		protected string Prefix => CmdHandler.GetPrefix(Context.Guild);
 
-        protected MitternachtTopLevelModule(bool isTopLevelModule = true)
-        {
-            //if it's top level module
-            ModuleTypeName = isTopLevelModule ? GetType().Name : GetType().DeclaringType.Name;
-            _log = LogManager.GetCurrentClassLogger();
-        }
+		protected MitternachtTopLevelModule(bool isTopLevelModule = true) {
+			ModuleTypeName = isTopLevelModule ? GetType().Name : GetType().DeclaringType.Name;
+			_log           = LogManager.GetCurrentClassLogger();
+		}
 
-        protected override void BeforeExecute(CommandInfo cmd)
-        {
-            CultureInfo = Localization.GetCultureInfo(Context.Guild?.Id);
-        }
+		protected override void BeforeExecute(CommandInfo cmd) {
+			CultureInfo = Localization.GetCultureInfo(Context.Guild?.Id);
+		}
 
-        //public Task<IUserMessage> ReplyConfirmLocalized(string titleKey, string textKey, string url = null, string footer = null)
-        //{
-        //    var title = MitternachtBot.ResponsesResourceManager.GetString(titleKey, cultureInfo);
-        //    var text = MitternachtBot.ResponsesResourceManager.GetString(textKey, cultureInfo);
-        //    return Context.Channel.SendConfirmAsync(title, text, url, footer);
-        //}
+		protected string GetText(string key)
+			=> Strings.GetText(key, CultureInfo, LowerModuleTypeName);
 
-        //public Task<IUserMessage> ReplyConfirmLocalized(string textKey)
-        //{
-        //    var text = MitternachtBot.ResponsesResourceManager.GetString(textKey, cultureInfo);
-        //    return Context.Channel.SendConfirmAsync(Context.User.Mention + " " + textKey);
-        //}
+		protected string GetText(string key, params object[] replacements)
+			=> Strings.GetText(key, CultureInfo, LowerModuleTypeName, replacements);
 
-        //public Task<IUserMessage> ReplyErrorLocalized(string titleKey, string textKey, string url = null, string footer = null)
-        //{
-        //    var title = MitternachtBot.ResponsesResourceManager.GetString(titleKey, cultureInfo);
-        //    var text = MitternachtBot.ResponsesResourceManager.GetString(textKey, cultureInfo);
-        //    return Context.Channel.SendErrorAsync(title, text, url, footer);
-        //}
-
-        protected string GetText(string key) =>
-            Strings.GetText(key, CultureInfo, LowerModuleTypeName);
-
-        protected string GetText(string key, params object[] replacements) =>
-            Strings.GetText(key, CultureInfo, LowerModuleTypeName, replacements);
-
-		public Task<IUserMessage> MessageLocalized(string textKey, params object[] replacements) {
+		protected Task<IUserMessage> MessageLocalized(string textKey, params object[] replacements) {
 			var text = GetText(textKey, replacements);
 			return Context.Channel.SendMessageAsync(text);
 		}
 
-		public Task<IUserMessage> ReplyLocalized(string textKey, params object[] replacements) {
+		protected Task<IUserMessage> ReplyLocalized(string textKey, params object[] replacements) {
 			var text = GetText(textKey, replacements);
 			return Context.Channel.SendMessageAsync($"{Context.User.Mention} {text}");
 		}
 
-        public Task<IUserMessage> ErrorLocalized(string textKey, params object[] replacements)
-        {
-            var text = GetText(textKey, replacements);
-            return Context.Channel.SendErrorAsync(text);
-        }
+		protected Task<IUserMessage> ErrorLocalized(string textKey, params object[] replacements) {
+			var text = GetText(textKey, replacements);
+			return Context.Channel.SendErrorAsync(text);
+		}
 
-        public Task<IUserMessage> ReplyErrorLocalized(string textKey, params object[] replacements)
-        {
-            var text = GetText(textKey, replacements);
-            return Context.Channel.SendErrorAsync($"{Context.User.Mention} {text}");
-        }
+		protected Task<IUserMessage> ReplyErrorLocalized(string textKey, params object[] replacements) {
+			var text = GetText(textKey, replacements);
+			return Context.Channel.SendErrorAsync($"{Context.User.Mention} {text}");
+		}
 
-        public Task<IUserMessage> ConfirmLocalized(string textKey, params object[] replacements)
-        {
-            var text = GetText(textKey, replacements);
-            return Context.Channel.SendConfirmAsync(text);
-        }
+		protected Task<IUserMessage> ConfirmLocalized(string textKey, params object[] replacements) {
+			var text = GetText(textKey, replacements);
+			return Context.Channel.SendConfirmAsync(text);
+		}
 
-        public Task<IUserMessage> ReplyConfirmLocalized(string textKey, params object[] replacements)
-        {
-            var text = GetText(textKey, replacements);
-            return Context.Channel.SendConfirmAsync($"{Context.User.Mention} {text}");
-        }
-        
-        // TypeConverter typeConverter = TypeDescriptor.GetConverter(propType); ?
-        public async Task<string> GetUserInputAsync(ulong userId, ulong channelId)
-        {
-            var userInputTask = new TaskCompletionSource<string>();
-            var dsc = (DiscordSocketClient)Context.Client;
-            try
-            {
-                dsc.MessageReceived += MessageReceived;
+		protected Task<IUserMessage> ReplyConfirmLocalized(string textKey, params object[] replacements) {
+			var text = GetText(textKey, replacements);
+			return Context.Channel.SendConfirmAsync($"{Context.User.Mention} {text}");
+		}
 
-                if ((await Task.WhenAny(userInputTask.Task, Task.Delay(10000))) != userInputTask.Task)
-                {
-                    return null;
-                }
+		protected async Task<string> GetUserInputAsync(ulong userId, ulong channelId) {
+			var userInputTask = new TaskCompletionSource<string>();
+			var dsc           = (DiscordSocketClient)Context.Client;
+			try {
+				dsc.MessageReceived += MessageReceived;
 
-                return await userInputTask.Task;
-            }
-            finally
-            {
-                dsc.MessageReceived -= MessageReceived;
-            }
+				if((await Task.WhenAny(userInputTask.Task, Task.Delay(10000))) != userInputTask.Task) {
+					return null;
+				}
 
-            Task MessageReceived(SocketMessage arg)
-            {
-                var _ = Task.Run(() =>
-                {
-                    if (!(arg is SocketUserMessage userMsg) ||
-                        !(userMsg.Channel is ITextChannel) ||
-                        userMsg.Author.Id != userId ||
-                        userMsg.Channel.Id != channelId)
-                    {
-                        return Task.CompletedTask;
-                    }
+				return await userInputTask.Task;
+			} finally {
+				dsc.MessageReceived -= MessageReceived;
+			}
 
-                    if (userInputTask.TrySetResult(arg.Content))
-                    {
-                        userMsg.DeleteAfter(1);
-                    }
-                    return Task.CompletedTask;
-                });
-                return Task.CompletedTask;
-            }
-        }
-    }
-    
-    public abstract class MitternachtTopLevelModule<TService> : MitternachtTopLevelModule where TService : INService
-    {
-        public TService Service { get; set; }
+			Task MessageReceived(SocketMessage arg) {
+				var _ = Task.Run(() => {
+					if(!(arg is SocketUserMessage userMsg) ||
+						!(userMsg.Channel is ITextChannel) ||
+						userMsg.Author.Id != userId ||
+						userMsg.Channel.Id != channelId) {
+						return Task.CompletedTask;
+					}
 
-        protected MitternachtTopLevelModule(bool isTopLevel = true) : base(isTopLevel)
-        {
-        }
-    }
+					if(userInputTask.TrySetResult(arg.Content)) {
+						userMsg.DeleteAfter(1);
+					}
 
-    public abstract class MitternachtSubmodule : MitternachtTopLevelModule
-    {
-        protected MitternachtSubmodule() : base(false) { }
-    }
+					return Task.CompletedTask;
+				});
+				return Task.CompletedTask;
+			}
+		}
+	}
 
-    public abstract class MitternachtSubmodule<TService> : MitternachtTopLevelModule<TService> where TService : INService
-    {
-        protected MitternachtSubmodule() : base(false)
-        {
-        }
-    }
+	public abstract class MitternachtTopLevelModule<TService> : MitternachtTopLevelModule where TService : INService {
+		public TService Service { get; set; }
+
+		protected MitternachtTopLevelModule(bool isTopLevel = true) : base(isTopLevel) { }
+	}
+
+	public abstract class MitternachtSubmodule : MitternachtTopLevelModule {
+		protected MitternachtSubmodule() : base(false) { }
+	}
+
+	public abstract class MitternachtSubmodule<TService> : MitternachtTopLevelModule<TService> where TService : INService {
+		protected MitternachtSubmodule() : base(false) { }
+	}
 }
