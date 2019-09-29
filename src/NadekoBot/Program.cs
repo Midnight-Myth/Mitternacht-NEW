82792fbd7005e21dab879a29da22c59f5e05476e
expeehaa <expeehaa@outlook.com>
2019-09-30 00:15:39 +0200

    Format Program.cs.

--- src/NadekoBot/Program.cs
+++ src/NadekoBot/Program.cs
@@ -1,15 +1,11 @@
-﻿namespace Mitternacht
-{
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            if (args.Length == 3 && int.TryParse(args[0], out var shardId) && int.TryParse(args[1], out var parentProcessId))
-            {
-                if (int.TryParse(args[2], out var outPort)) { }
-                new MitternachtBot(shardId, parentProcessId, outPort).RunAndBlockAsync(args).GetAwaiter().GetResult();
-            }
-            else new MitternachtBot(0, 0).RunAndBlockAsync(args).GetAwaiter().GetResult();
-        }
-    }
+﻿namespace Mitternacht {
+	public class Program {
+		public static void Main(string[] args) {
+			if(args.Length == 3 && int.TryParse(args[0], out var shardId) && int.TryParse(args[1], out var parentProcessId)) {
+				if(int.TryParse(args[2], out var outPort)) { }
+
+				new MitternachtBot(shardId, parentProcessId, outPort).RunAndBlockAsync(args).GetAwaiter().GetResult();
+			} else new MitternachtBot(0, 0).RunAndBlockAsync(args).GetAwaiter().GetResult();
+		}
+	}
 }
