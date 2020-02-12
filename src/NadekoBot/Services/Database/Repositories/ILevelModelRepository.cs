6f2bd24861f19708334bfd97f89eaa4f3db35ecb
expeehaa <expeehaa@outlook.com>
2020-02-12 22:36:46 +0100

    Remove useless comments.

--- src/NadekoBot/Services/Database/Repositories/ILevelModelRepository.cs
+++ src/NadekoBot/Services/Database/Repositories/ILevelModelRepository.cs
@@ -7,58 +7,11 @@ namespace Mitternacht.Services.Database.Repositories
     public interface ILevelModelRepository : IRepository<LevelModel>
     {
         LevelModel GetOrCreate(ulong guildId, ulong userId);
-
-        /// <summary>
-        /// Gets the LevelModel for a given user.
-        /// </summary>
-        /// <param name="guildId">ID of the user's guild</param>
-        /// <param name="userId">The user ID of the LevelModel.</param>
-        /// <returns>The LevelModel of the given user or null.</returns>
         LevelModel Get(ulong guildId, ulong userId);
-
-        /// <summary>
-        /// Add XP to a given user.
-        /// </summary>
-        /// <param name="guildId"></param>
-        /// <param name="userId">The user ID of the user receiving the XP.</param>
-        /// <param name="xp">A specified amount of XP to add. Can be negative.</param>
-        /// <param name="channelId"></param>
-        void AddXp(ulong guildId, ulong userId, int xp, ulong? channelId = null);
-
-        /// <summary>
-        /// Set the XP of a specified user.
-        /// </summary>
-        /// <param name="guildId"></param>
-        /// <param name="userId">The id of the user.</param>
-        /// <param name="xp">The new amount of xp.</param>
-        /// <param name="channelId"></param>
+		void AddXp(ulong guildId, ulong userId, int xp, ulong? channelId = null);
         void SetXp(ulong guildId, ulong userId, int xp, ulong? channelId = null);
-
-        /// <summary>
-        /// Set the XP of a specified user.
-        /// </summary>
-        /// <param name="guildId"></param>
-        /// <param name="userId">The id of the user.</param>
-        /// <param name="level"></param>
-        /// <param name="channelId"></param>
-        void SetLevel(ulong guildId, ulong userId, int level, ulong? channelId = null);
-
-        /// <summary>
-        /// Decides wether a user can get Message XP or not.
-        /// </summary>
-        /// <param name="guildId"></param>
-        /// <param name="userId">The users ID.</param>
-        /// <param name="time">The time to compare with the saved timestamp.</param>
-        /// <returns>True, if the user can get Message XP.</returns>
-        bool CanGetMessageXp(ulong guildId, ulong userId, DateTime time);
-
-        /// <summary>
-        /// Replaces the current timestamp with a new one.
-        /// </summary>
-        /// <param name="guildId"></param>
-        /// <param name="userId">The users ID.</param>
-        /// <param name="timestamp">The new Timestamp</param>
-        /// <returns>True, if the timestamp was successfully replaced.</returns>
+		void SetLevel(ulong guildId, ulong userId, int level, ulong? channelId = null);
+		bool CanGetMessageXp(ulong guildId, ulong userId, DateTime time);
         void ReplaceTimestamp(ulong guildId, ulong userId, DateTime timestamp);
 		
 		IOrderedQueryable<LevelModel> GetAllSortedForRanks(ulong guildId, ulong[] guildUserIds);
