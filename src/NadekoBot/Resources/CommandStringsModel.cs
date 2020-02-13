fb8fa0d9ec34fab2390690f3aea51868853453bd
expeehaa <expeehaa@outlook.com>
2020-02-13 01:42:32 +0100

    Use YAML instead of JSON for command strings.

--- src/NadekoBot/Resources/CommandStringsModel.cs
+++ src/NadekoBot/Resources/CommandStringsModel.cs
@@ -1,22 +1,20 @@
-﻿using Newtonsoft.Json;
+﻿using YamlDotNet.Serialization;
 
-namespace Mitternacht.Resources
-{
-    public class CommandStringsModel
-    {
-        [JsonProperty("name")]
-        public string Name { get; set; } = "";
+namespace Mitternacht.Resources {
+	public class CommandStringsModel {
+		[YamlMember(Alias = "name")]
+		public string Name { get; set; } = "";
 
-        [JsonProperty("cmd")]
-        public string Command { get; set; } = "";
+		[YamlMember(Alias = "cmd")]
+		public string Command { get; set; } = "";
 
-        [JsonProperty("aliases")]
-        public string[] Aliases { get; set; } = { };
+		[YamlMember(Alias = "aliases")]
+		public string[] Aliases { get; set; } = { };
 
-        [JsonProperty("desc")]
-        public string Description { get; set; } = "";
+		[YamlMember(Alias = "desc")]
+		public string Description { get; set; } = "";
 
-        [JsonProperty("usage")]
-        public string Usage { get; set; } = "";
-    }
+		[YamlMember(Alias = "usage")]
+		public string Usage { get; set; } = "";
+	}
 }
