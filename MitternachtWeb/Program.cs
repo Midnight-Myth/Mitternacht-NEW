651d25e5caffdab3b4cb2f081dd2ec0036a4d965
expeehaa <expeehaa@outlook.com>
2020-04-18 01:59:38 +0200

    Start MitternachtBot when starting MitternachtWeb.

--- MitternachtWeb/Program.cs
+++ MitternachtWeb/Program.cs
@@ -1,10 +1,14 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
+using Mitternacht;
+using System.Threading.Tasks;
 
 namespace MitternachtWeb {
 	public class Program {
-		public static void Main(string[] args) {
-			CreateHostBuilder(args).Build().Run();
+		public static async Task Main(string[] args) {
+			await new MitternachtBot(0, 0).RunAsync(args);
+			
+			await CreateHostBuilder(args).Build().RunAsync();
 		}
 
 		public static IHostBuilder CreateHostBuilder(string[] args)
