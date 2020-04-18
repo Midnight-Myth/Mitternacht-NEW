4395d77667afd7a9106a7a37d08531bcc14d3a5b
expeehaa <expeehaa@outlook.com>
2020-04-18 21:26:38 +0200

    Add the MitternachtBot instance and its services to the ASP.NET services.

--- MitternachtWeb/Startup.cs
+++ MitternachtWeb/Startup.cs
@@ -2,7 +2,9 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
 using Microsoft.Extensions.Hosting;
+using System.Linq;
 
 namespace MitternachtWeb {
 	public class Startup {
@@ -14,6 +16,8 @@ namespace MitternachtWeb {
 
 		public void ConfigureServices(IServiceCollection services) {
 			services.AddControllersWithViews();
+			services.Add(ServiceDescriptor.Singleton(Program.MitternachtBot));
+			services.Add(Program.MitternachtBot.Services.Services.Select(s => ServiceDescriptor.Singleton(s.Key, s.Value)));
 		}
 
 		public void Configure(IApplicationBuilder app, IWebHostEnvironment env) {
