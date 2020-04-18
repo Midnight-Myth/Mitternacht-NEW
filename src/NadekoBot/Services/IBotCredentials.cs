e35059265c5529410f2ac3ab40ec4dea10a5e33a
expeehaa <expeehaa@outlook.com>
2020-04-18 02:25:01 +0200

    Apply improved formatting.

--- src/NadekoBot/Services/IBotCredentials.cs
+++ src/NadekoBot/Services/IBotCredentials.cs
@@ -2,30 +2,29 @@
 using Discord;
 using Mitternacht.Common;
 
-namespace Mitternacht.Services
-{
-    public interface IBotCredentials
-    {
-        ulong ClientId { get; }
+namespace Mitternacht.Services {
+	public interface IBotCredentials {
+		ulong                 ClientId           { get; }
+		string                Token              { get; }
+		ImmutableArray<ulong> OwnerIds           { get; }
+		DbConfig              Db                 { get; }
+		
+		string                GoogleApiKey       { get; }
+		string                MashapeKey         { get; }
+		string                LoLApiKey          { get; }
+		string                OsuApiKey          { get; }
+		string                CleverbotApiKey    { get; }
+		string                CarbonKey          { get; }
+		string                PatreonCampaignId  { get; }
+		string                PatreonAccessToken { get; }
+		
+		int                   TotalShards        { get; }
+		string                ShardRunCommand    { get; }
+		string                ShardRunArguments  { get; }
+		
+		string                ForumUsername      { get; }
+		string                ForumPassword      { get; }
 
-        string Token { get; }
-        string GoogleApiKey { get; }
-        ImmutableArray<ulong> OwnerIds { get; }
-        string MashapeKey { get; }
-        string LoLApiKey { get; }
-        string PatreonAccessToken { get; }
-        string CarbonKey { get; }
-
-        DbConfig Db { get; }
-        string OsuApiKey { get; }
-
-        bool IsOwner(IUser u);
-        int TotalShards { get; }
-        string ShardRunCommand { get; }
-        string ShardRunArguments { get; }
-        string PatreonCampaignId { get; }
-        string CleverbotApiKey { get; }
-        string ForumUsername { get; }
-        string ForumPassword { get; }
-    }
+		bool IsOwner(IUser u);
+	}
 }
