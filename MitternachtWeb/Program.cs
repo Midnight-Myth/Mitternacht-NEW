6ca3a919a6807df1b3d336c8a0154fa6f7cb7cbd
expeehaa <expeehaa@outlook.com>
2020-04-19 16:18:02 +0200

    Require config file "mitternachtweb.config" and add an example file.

--- MitternachtWeb/Program.cs
+++ MitternachtWeb/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Mitternacht;
 using System.Threading.Tasks;
@@ -15,6 +16,8 @@ namespace MitternachtWeb {
 		}
 
 		public static IHostBuilder CreateHostBuilder(string[] args)
-			=> Host.CreateDefaultBuilder(args).ConfigureWebHostDefaults(webBuilder => webBuilder.UseStartup<Startup>());
+			=> Host.CreateDefaultBuilder(args).ConfigureAppConfiguration((context, config) => {
+				config.AddJsonFile("mitternachtweb.config");
+			}).ConfigureWebHostDefaults(webBuilder => webBuilder.UseStartup<Startup>());
 	}
 }
