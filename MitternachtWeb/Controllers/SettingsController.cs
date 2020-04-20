41fcdad1101df3c03e6756b8cd281f9949fa5067
expeehaa <expeehaa@outlook.com>
2020-04-20 15:40:15 +0200

    Add Settings controller with pages and layout.

---
+++ MitternachtWeb/Controllers/SettingsController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MitternachtWeb.Controllers {
+	[Authorize]
+	public class SettingsController : Controller {
+		public IActionResult Index() {
+			return View();
+		}
+	}
+}
