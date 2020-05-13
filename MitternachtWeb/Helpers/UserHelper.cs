d7fa06040aaa818cf3581a5613e9ae2e94c2ff8a
expeehaa <expeehaa@outlook.com>
2020-05-13 18:46:27 +0200

    Change line endings of "*.cs" files to lf in MitternachtWeb project.

--- MitternachtWeb/Helpers/UserHelper.cs
+++ MitternachtWeb/Helpers/UserHelper.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Http;
 using MitternachtWeb.Models;
 using Newtonsoft.Json;
--- MitternachtWeb/Models/PagePermission.cs
+++ MitternachtWeb/Models/PagePermission.cs
@@ -1,4 +1,4 @@
-﻿using Discord;
+using Discord;
 using System;
 
 namespace MitternachtWeb.Models {
