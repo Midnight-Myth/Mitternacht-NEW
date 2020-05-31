26b26863538341fd067488ba5f05e56e5c74019a
expeehaa <expeehaa@outlook.com>
2020-05-31 15:53:41 +0200

    Format file and simplify code.

--- src/MitternachtBot/Modules/Administration/Services/GuildTimezoneService.cs
+++ src/MitternachtBot/Modules/Administration/Services/GuildTimezoneService.cs
@@ -7,68 +7,51 @@ using Mitternacht.Extensions;
 using Mitternacht.Services;
 using Mitternacht.Services.Database.Models;
 
-namespace Mitternacht.Modules.Administration.Services
-{
-    public class GuildTimezoneService : IMService
-    {
-        // todo 70 this is a hack >.<
-        public static ConcurrentDictionary<ulong, GuildTimezoneService> AllServices { get; } = new ConcurrentDictionary<ulong, GuildTimezoneService>();
-        private ConcurrentDictionary<ulong, TimeZoneInfo> _timezones;
-        private readonly DbService _db;
+namespace Mitternacht.Modules.Administration.Services {
+	public class GuildTimezoneService : IMService {
+		// todo 70 this is a hack >.<
+		public static ConcurrentDictionary<ulong, GuildTimezoneService> AllServices { get; } = new ConcurrentDictionary<ulong, GuildTimezoneService>();
+		private ConcurrentDictionary<ulong, TimeZoneInfo> _timezones;
+		private readonly DbService _db;
 
-        public GuildTimezoneService(DiscordSocketClient client, IEnumerable<GuildConfig> gcs, DbService db)
-        {
-            _timezones = gcs
-                .Select(x =>
-                {
-                    TimeZoneInfo tz;
-                    try
-                    {
-                        if (x.TimeZoneId == null)
-                            tz = null;
-                        else
-                            tz = TimeZoneInfo.FindSystemTimeZoneById(x.TimeZoneId);
-                    }
-                    catch
-                    {
-                        tz = null;
-                    }
-                    return (x.GuildId, tz);
-                })
-                .Where(x => x.Item2 != null)
-                .ToDictionary(x => x.Item1, x => x.Item2)
-                .ToConcurrent();
+		public GuildTimezoneService(DiscordSocketClient client, IEnumerable<GuildConfig> gcs, DbService db) {
+			_timezones = gcs
+				.Select(x => {
+					TimeZoneInfo tz;
+					try {
+						tz = x.TimeZoneId == null ? null : TimeZoneInfo.FindSystemTimeZoneById(x.TimeZoneId);
+					} catch {
+						tz = null;
+					}
+					return (x.GuildId, tz);
+				})
+				.Where(x => x.tz != null)
+				.ToDictionary(x => x.GuildId, x => x.tz)
+				.ToConcurrent();
 
-            var curUser = client.CurrentUser;
-            if (curUser != null)
-                AllServices.TryAdd(curUser.Id, this);
-            _db = db;
-        }
+			var curUser = client.CurrentUser;
+			if(curUser != null)
+				AllServices.TryAdd(curUser.Id, this);
+			_db = db;
+		}
 
-        public TimeZoneInfo GetTimeZoneOrDefault(ulong guildId)
-        {
-            if (_timezones.TryGetValue(guildId, out var tz))
-                return tz;
-            return null;
-        }
+		public TimeZoneInfo GetTimeZoneOrDefault(ulong guildId)
+			=> _timezones.TryGetValue(guildId, out var tz) ? tz : null;
 
-        public void SetTimeZone(ulong guildId, TimeZoneInfo tz)
-        {
-            using (var uow = _db.UnitOfWork)
-            {
-                var gc = uow.GuildConfigs.For(guildId, set => set);
+		public void SetTimeZone(ulong guildId, TimeZoneInfo tz) {
+			using var uow = _db.UnitOfWork;
+			var gc = uow.GuildConfigs.For(guildId, set => set);
 
-                gc.TimeZoneId = tz?.Id;
-                uow.Complete();
+			gc.TimeZoneId = tz?.Id;
+			uow.Complete();
 
-                if (tz == null)
-                    _timezones.TryRemove(guildId, out tz);
-                else
-                    _timezones.AddOrUpdate(guildId, tz, (key, old) => tz);
-            }
-        }
+			if(tz == null)
+				_timezones.TryRemove(guildId, out tz);
+			else
+				_timezones.AddOrUpdate(guildId, tz, (key, old) => tz);
+		}
 
-        public TimeZoneInfo GetTimeZoneOrUtc(ulong guildId)
-            => GetTimeZoneOrDefault(guildId) ?? TimeZoneInfo.Utc;
-    }
+		public TimeZoneInfo GetTimeZoneOrUtc(ulong guildId)
+			=> GetTimeZoneOrDefault(guildId) ?? TimeZoneInfo.Utc;
+	}
 }
