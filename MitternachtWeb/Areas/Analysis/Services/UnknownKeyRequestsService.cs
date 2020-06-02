b654d10113c5ad3fca38ad7f428232928abe6df0
expeehaa <expeehaa@outlook.com>
2020-06-02 13:25:02 +0200

    Fix firing of UnknownKeyRequested event and initialization of UnknownKeyRequestsService.

--- MitternachtWeb/Areas/Analysis/Services/UnknownKeyRequestsService.cs
+++ MitternachtWeb/Areas/Analysis/Services/UnknownKeyRequestsService.cs
@@ -6,11 +6,15 @@ namespace MitternachtWeb.Areas.Analysis.Services {
 	public class UnknownKeyRequestsService {
 		public ConcurrentDictionary<(string moduleName, string key, string cultureName), ulong> UnknownKeyRequests = new ConcurrentDictionary<(string moduleName, string key, string cultureName), ulong>();
 
+		private readonly StringService _stringService;
+
 		public UnknownKeyRequestsService(StringService ss) {
-			ss.UnknownKeyRequested += UnknownKeyRequested;
+			_stringService = ss;
+
+			_stringService.UnknownKeyRequested += UnknownKeyRequested;
 		}
 
-		private void UnknownKeyRequested(string moduleName, string key, CultureInfo cultureInfo) {
+		public void UnknownKeyRequested(string moduleName, string key, CultureInfo cultureInfo) {
 			UnknownKeyRequests.AddOrUpdate((moduleName, key, cultureInfo.Name), 1, (_, occurences) => occurences + 1);
 		}
 	}
