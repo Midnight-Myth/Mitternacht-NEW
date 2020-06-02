3eef20c4d566e1ce4fc594f413feb8280275ddb3
expeehaa <expeehaa@outlook.com>
2020-06-02 04:28:46 +0200

    Add area for analysis views and page for unknown requested translation keys.

---
+++ MitternachtWeb/Areas/Analysis/Controllers/AnalysisController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using MitternachtWeb.Controllers;
+
+namespace MitternachtWeb.Areas.Analysis.Controllers {
+	public class AnalysisController : DiscordUserController {
+		[Authorize]
+		[Area("Analysis")]
+		public IActionResult Index() {
+			return View();
+		}
+	}
+}
---
+++ MitternachtWeb/Areas/Analysis/Controllers/UnknownKeyRequestsController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using MitternachtWeb.Areas.Analysis.Services;
+using MitternachtWeb.Controllers;
+using System.Linq;
+
+namespace MitternachtWeb.Areas.Analysis.Controllers {
+	public class UnknownKeyRequestsController : DiscordUserController {
+		private readonly UnknownKeyRequestsService _unknownKeyRequestsService;
+
+		public UnknownKeyRequestsController(UnknownKeyRequestsService ukrs) {
+			_unknownKeyRequestsService = ukrs;
+		}
+
+		[Authorize]
+		[Area("Analysis")]
+		public IActionResult Index() {
+			return View(_unknownKeyRequestsService.UnknownKeyRequests.Select(kv => (kv.Key.moduleName, kv.Key.key, kv.Key.cultureName, kv.Value)));
+		}
+	}
+}
