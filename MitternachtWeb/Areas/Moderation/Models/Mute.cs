895191853e92e7ef2eda132423f48d3152c300cf
expeehaa <expeehaa@outlook.com>
2020-06-03 17:06:10 +0200

    Add username to muted users table.

--- MitternachtWeb/Areas/Moderation/Models/Mute.cs
+++ MitternachtWeb/Areas/Moderation/Models/Mute.cs
@@ -5,6 +5,7 @@ namespace MitternachtWeb.Areas.Moderation.Models {
 		public ulong     UserId   { get; set; }
 		public bool      Muted    { get; set; }
 		public DateTime? UnmuteAt { get; set; }
+		public string    Username { get; set; }
 
 		public string MutedUntil => UnmuteAt.HasValue ? UnmuteAt.Value.ToString("yyyy-MM-ddTHH:mm:ssZ") : "-";
 	}
