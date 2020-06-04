7c040dbe1e974c124169a52b4cd3651f4dda8e5c
expeehaa <expeehaa@outlook.com>
2020-06-05 01:25:12 +0200

    Add page listing quotes for a guild.

---
+++ MitternachtWeb/Areas/Guild/Controllers/QuotesController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Mitternacht.Services;
+using MitternachtWeb.Areas.Guild.Models;
+using System.Linq;
+
+namespace MitternachtWeb.Areas.Guild.Controllers {
+	[Authorize]
+	[Area("Guild")]
+	public class QuotesController : GuildBaseController {
+		private readonly DbService _db;
+		
+		public QuotesController(DbService db) {
+			_db = db;
+		}
+		
+		public IActionResult Index() {
+			using var uow = _db.UnitOfWork;
+			var quotes = uow.Quotes.GetAllForGuild(GuildId).Select(q => {
+				var user = Guild.GetUser(q.AuthorId);
+
+				return new Quote {
+					Id         = q.Id,
+					AuthorId   = q.AuthorId,
+					Authorname = user?.ToString() ?? uow.UsernameHistory.GetUsernamesDescending(q.AuthorId).FirstOrDefault()?.ToString() ?? q.AuthorName ?? "-",
+					AvatarUrl  = user?.GetAvatarUrl() ?? user?.GetDefaultAvatarUrl(),
+					Keyword    = q.Keyword,
+					Text       = q.Text,
+					AddedAt    = q.DateAdded,
+				};
+			}).ToList();
+
+			return View(quotes);
+		}
+	}
+}
---
+++ MitternachtWeb/Areas/Guild/Models/Quote.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MitternachtWeb.Areas.Guild.Models {
+	public class Quote {
+		public int       Id         { get; set; }
+		public ulong     AuthorId   { get; set; }
+		public string    Authorname { get; set; }
+		public string    AvatarUrl  { get; set; }
+		public string    Keyword    { get; set; }
+		public string    Text       { get; set; }
+		public DateTime? AddedAt    { get; set; }
+	}
+}
