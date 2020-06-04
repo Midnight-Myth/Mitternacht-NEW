fddd0b47804d9e872ec09a13d5a376e898ae1bf7
expeehaa <expeehaa@outlook.com>
2020-06-04 20:42:48 +0200

    Format files.

--- src/MitternachtBot/Services/Database/Repositories/IWarningsRepository.cs
+++ src/MitternachtBot/Services/Database/Repositories/IWarningsRepository.cs
@@ -1,12 +1,10 @@
 using System.Threading.Tasks;
 using Mitternacht.Services.Database.Models;
 
-namespace Mitternacht.Services.Database.Repositories
-{
-    public interface IWarningsRepository : IRepository<Warning>
-    {
-        Warning[] For(ulong guildId, ulong userId);
-        Task ForgiveAll(ulong guildId, ulong userId, string mod);
-        Warning[] GetForGuild(ulong id);
-    }
+namespace Mitternacht.Services.Database.Repositories {
+	public interface IWarningsRepository : IRepository<Warning> {
+		Warning[] For(ulong guildId, ulong userId);
+		Task ForgiveAll(ulong guildId, ulong userId, string mod);
+		Warning[] GetForGuild(ulong id);
+	}
 }
--- src/MitternachtBot/Services/Database/Repositories/Impl/WarningsRepository.cs
+++ src/MitternachtBot/Services/Database/Repositories/Impl/WarningsRepository.cs
@@ -5,37 +5,25 @@ using Mitternacht.Services.Database.Models;
 using System;
 using System.Linq.Expressions;
 
-namespace Mitternacht.Services.Database.Repositories.Impl
-{
-    public class WarningsRepository : Repository<Warning>, IWarningsRepository
-    {
-        public WarningsRepository(DbContext context) : base(context)
-        {
-        }
+namespace Mitternacht.Services.Database.Repositories.Impl {
+	public class WarningsRepository : Repository<Warning>, IWarningsRepository {
+		public WarningsRepository(DbContext context) : base(context) { }
 
-        public Warning[] For(ulong guildId, ulong userId)
-        {
-            var query = _set.Where((Expression<Func<Warning, bool>>)(x => x.GuildId == guildId && x.UserId == userId))
-                .OrderByDescending(x => x.DateAdded);
+		public Warning[] For(ulong guildId, ulong userId)
+			=> _set.Where((Expression<Func<Warning, bool>>)(x => x.GuildId == guildId && x.UserId == userId)).OrderByDescending(x => x.DateAdded).ToArray();
 
-            return query.ToArray();
-        }
-
-        public async Task ForgiveAll(ulong guildId, ulong userId, string mod)
-        {
-            await _set.Where((Expression<Func<Warning, bool>>)(x => x.GuildId == guildId && x.UserId == userId))
-                .ForEachAsync(x =>
-                {
-					if(x.Forgiven) return;
-					x.Forgiven   = true;
+		public async Task ForgiveAll(ulong guildId, ulong userId, string mod) {
+			await _set.Where((Expression<Func<Warning, bool>>)(x => x.GuildId == guildId && x.UserId == userId))
+				.ForEachAsync(x => {
+					if(x.Forgiven)
+						return;
+					x.Forgiven = true;
 					x.ForgivenBy = mod;
 				})
-                .ConfigureAwait(false);
-        }
+				.ConfigureAwait(false);
+		}
 
-        public Warning[] GetForGuild(ulong id)
-        {
-            return _set.Where((Expression<Func<Warning, bool>>)(x => x.GuildId == id)).ToArray();
-        }
-    }
+		public Warning[] GetForGuild(ulong id)
+			=> _set.Where((Expression<Func<Warning, bool>>)(x => x.GuildId == id)).ToArray();
+	}
 }
