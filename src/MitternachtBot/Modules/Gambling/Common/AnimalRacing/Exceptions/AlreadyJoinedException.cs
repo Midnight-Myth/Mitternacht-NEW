170552fcca589b5ae1699ec6702f4eedb4fec662
expeehaa <expeehaa@outlook.com>
2020-06-09 02:31:11 +0200

    Remove many submodules of Gambling.

--- src/MitternachtBot/Modules/Gambling/Common/AnimalRacing/Exceptions/AlreadyJoinedException.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-namespace Mitternacht.Modules.Gambling.Common.AnimalRacing.Exceptions
-{
-    public class AlreadyJoinedException : Exception
-    {
-
-    }
-}
--- src/MitternachtBot/Modules/Gambling/Common/AnimalRacing/Exceptions/AnimalRaceFullException.cs
+++
@@ -1,8 +0,0 @@
-using System;
-
-namespace Mitternacht.Modules.Gambling.Common.AnimalRacing.Exceptions
-{
-    public class AnimalRaceFullException : Exception
-    {
-    }
-}
