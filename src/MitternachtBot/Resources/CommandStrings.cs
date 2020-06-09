15dc12f80de05a67675bad8af9bf65ea8a5fb048
expeehaa <expeehaa@outlook.com>
2020-06-09 02:54:20 +0200

    Format file.

--- src/MitternachtBot/Resources/CommandStrings.cs
+++ src/MitternachtBot/Resources/CommandStrings.cs
@@ -6,29 +6,27 @@ using NLog;
 using YamlDotNet.Serialization;
 
 namespace Mitternacht.Resources {
-	public class CommandStrings
-    {
-        private static readonly Logger Log;
-        private const string CmdStringPath = @"./_strings/commandstrings.yml";
-        private static ConcurrentHashSet<CommandStringsModel> _commandStrings;
+	public class CommandStrings {
+		private static readonly Logger Log;
+		private const string CmdStringPath = @"./_strings/commandstrings.yml";
+		private static ConcurrentHashSet<CommandStringsModel> _commandStrings;
 
-        static CommandStrings() {
-            Log = LogManager.GetCurrentClassLogger();
-            LoadCommandStrings();
-        }
+		static CommandStrings() {
+			Log = LogManager.GetCurrentClassLogger();
+			LoadCommandStrings();
+		}
 
-        public static void LoadCommandStrings() {
-            try {
-				var yml = File.ReadAllText(CmdStringPath);
+		public static void LoadCommandStrings() {
+			try {
+				var yml          = File.ReadAllText(CmdStringPath);
 				var deserializer = new Deserializer();
-				_commandStrings = deserializer.Deserialize<ConcurrentHashSet<CommandStringsModel>>(yml);
-            }
-            catch (Exception e) {
+				_commandStrings  = deserializer.Deserialize<ConcurrentHashSet<CommandStringsModel>>(yml);
+			} catch(Exception e) {
 				Log.Error(e);
 			}
 		}
 
-        public static CommandStringsModel GetCommandStringModel(string name) 
-            => _commandStrings.FirstOrDefault(c => c.Name == name) ?? new CommandStringsModel { Name = name, Command = name + "_cmd" };
-    }
+		public static CommandStringsModel GetCommandStringModel(string name)
+			=> _commandStrings.FirstOrDefault(c => c.Name == name) ?? new CommandStringsModel { Name = name, Command = $"{name}_cmd" };
+	}
 }
