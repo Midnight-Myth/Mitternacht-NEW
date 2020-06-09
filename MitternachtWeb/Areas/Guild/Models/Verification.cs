8a3dc586b307d1d4e9294e5f85d47ded6be0c13b
expeehaa <expeehaa@outlook.com>
2020-06-09 23:29:10 +0200

    Add guild page showing current verifications.

---
+++ MitternachtWeb/Areas/Guild/Models/Verification.cs
@@ -0,0 +1,7 @@
+﻿namespace MitternachtWeb.Areas.Guild.Models {
+	public class Verification {
+		public string Username        { get; set; }
+		public string AvatarUrl       { get; set; }
+		public string ForumProfileUrl { get; set; }
+	}
+}
--- MitternachtWeb/Models/PagePermission.cs
+++ MitternachtWeb/Models/PagePermission.cs
@@ -4,48 +4,47 @@ using System;
 namespace MitternachtWeb.Models {
 	[Flags]
 	public enum BotLevelPermission {
-		None                 = 0b_0000_0000_0000,
-
-		ReadBotConfig        = 0b_0000_0000_0001,
-		WriteBotConfig       = 0b_0000_0000_0011,
-		
-		ReadAllGuildConfigs  = 0b_0000_0000_0100,
-		WriteAllGuildConfigs = 0b_0000_0000_1100,
-		
-		ReadAllMutes         = 0b_0000_0001_0000,
-		WriteAllMutes        = 0b_0000_0011_0000,
-		
-		ReadAllWarns         = 0b_0000_0100_0000,
-		ForgiveAllWarns      = 0b_0000_1100_0000,
-		WriteAllWarns        = 0b_0001_1100_0000,
-		
-		ReadAllQuotes        = 0b_0010_0000_0000,
-		WriteAllQuotes       = 0b_0110_0001_0000,
-
-		ReadAllGuilds        = ReadAllGuildConfigs | ReadAllMutes | ReadAllWarns | ReadAllQuotes,
-		ReadAll              = ReadBotConfig | ReadAllGuilds,
-		All                  = 0b_1111_1111_1111,
+		None                  = 0b_0000_0000_0000_0000,
+		ReadBotConfig         = 0b_0000_0000_0000_0001,
+		WriteBotConfig        = 0b_0000_0000_0000_0011,
+		ReadAllGuildConfigs   = 0b_0000_0000_0000_0100,
+		WriteAllGuildConfigs  = 0b_0000_0000_0000_1100,
+		ReadAllMutes          = 0b_0000_0000_0001_0000,
+		WriteAllMutes         = 0b_0000_0000_0011_0000,
+		ReadAllWarns          = 0b_0000_0000_0100_0000,
+		ForgiveAllWarns       = 0b_0000_0000_1100_0000,
+		WriteAllWarns         = 0b_0000_0001_1100_0000,
+		ReadAllQuotes         = 0b_0000_0010_0000_0000,
+		WriteAllQuotes        = 0b_0000_0110_0001_0000,
+		ReadAllVerifications  = 0b_0000_1000_0000_0000,
+		WriteAllVerifications = 0b_0001_1000_0000_0000,
+		ReadAllGuilds         = ReadAllGuildConfigs | ReadAllMutes | ReadAllWarns | ReadAllQuotes | ReadAllVerifications,
+		ReadAll               = ReadBotConfig | ReadAllGuilds,
+		All                   = 0b_1111_1111_1111_1111,
 	}
 
 	[Flags]
 	public enum GuildLevelPermission {
-		None             = 0b_0000_0000_0000,
+		None               = 0b_0000_0000_0000,
 
-		ReadGuildConfig  = 0b_0000_0000_0001,
-		WriteGuildConfig = 0b_0000_0000_0011,
+		ReadGuildConfig    = 0b_0000_0000_0001,
+		WriteGuildConfig   = 0b_0000_0000_0011,
 		
-		ReadMutes        = 0b_0000_0000_0100,
-		WriteMutes       = 0b_0000_0000_1100,
+		ReadMutes          = 0b_0000_0000_0100,
+		WriteMutes         = 0b_0000_0000_1100,
 
-		ReadWarns        = 0b_0000_0001_0000,
-		ForgiveWarns     = 0b_0000_0011_0000,
-		WriteWarns       = 0b_0000_0111_0000,
+		ReadWarns          = 0b_0000_0001_0000,
+		ForgiveWarns       = 0b_0000_0011_0000,
+		WriteWarns         = 0b_0000_0111_0000,
 
-		ReadQuotes       = 0b_0000_1000_0000,
-		WriteQuotes      = 0b_0001_1000_0100,
-		
-		ReadAll          = ReadGuildConfig | ReadMutes | ReadWarns | ReadQuotes,
-		All              = 0b_1111_1111_1111,
+		ReadQuotes         = 0b_0000_1000_0000,
+		WriteQuotes        = 0b_0001_1000_0100,
+
+		ReadVerifications  = 0b_0010_0000_0000,
+		WriteVerifications = 0b_0110_0000_0000,
+
+		ReadAll            = ReadGuildConfig | ReadMutes | ReadWarns | ReadQuotes | ReadVerifications,
+		All                = 0b_1111_1111_1111,
 	}
 
 	public static class PagePermissionExtensions {
@@ -61,7 +60,7 @@ namespace MitternachtWeb.Models {
 			}
 
 			if(guildPerms.BanMembers) {
-				perms |= GuildLevelPermission.WriteWarns;
+				perms |= GuildLevelPermission.WriteWarns | GuildLevelPermission.WriteVerifications;
 			}
 			
 			if(guildPerms.Administrator) {
