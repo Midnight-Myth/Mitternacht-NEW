3c5b0f212e6625dbb3c9508b244685b23f78815a
expeehaa <expeehaa@outlook.com>
2020-06-29 22:47:43 +0200

    Format extension files.

--- src/MitternachtBot/Extensions/MusicExtensions.cs
+++ src/MitternachtBot/Extensions/MusicExtensions.cs
@@ -1,10 +1,8 @@
 using Discord;
 
-namespace Mitternacht.Extensions
-{
-    public static class MusicExtensions
-    {
-        public static EmbedAuthorBuilder WithMusicIcon(this EmbedAuthorBuilder eab) 
-            => eab.WithIconUrl("http://i.imgur.com/nhKS3PT.png");
-    }
+namespace Mitternacht.Extensions {
+	public static class MusicExtensions {
+		public static EmbedAuthorBuilder WithMusicIcon(this EmbedAuthorBuilder eab)
+			=> eab.WithIconUrl("http://i.imgur.com/nhKS3PT.png");
+	}
 }
