e0df76f71a48f3c90e4d0ecf147d7757ec7d25ca
expeehaa <expeehaa@outlook.com>
2020-06-29 22:11:39 +0200

    Improve ShardCom files.

--- src/MitternachtBot/Common/ShardCom/ShardComServer.cs
+++ src/MitternachtBot/Common/ShardCom/ShardComServer.cs
@@ -5,36 +5,29 @@ using System.Text;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
 
-namespace Mitternacht.Common.ShardCom
-{
-    public class ShardComServer : IDisposable
-    {
-        private readonly UdpClient _client;
+namespace Mitternacht.Common.ShardCom {
+	public class ShardComServer : IDisposable {
+		private readonly UdpClient _client;
 
-        public ShardComServer(int port)
-        {
-            _client = new UdpClient(port);
-        }
+		public ShardComServer(int port) {
+			_client = new UdpClient(port);
+		}
 
-        public void Start()
-        {
-            Task.Run(async () =>
-            {
-                var ip = new IPEndPoint(IPAddress.Any, 0);
-                while (true)
-                {
-                    var recv = await _client.ReceiveAsync();
-                    var data = Encoding.UTF8.GetString(recv.Buffer);
-                    var _ = OnDataReceived(JsonConvert.DeserializeObject<ShardComMessage>(data));
-                }
-            });
-        }
+		public void Start() {
+			Task.Run(async () => {
+				var ip = new IPEndPoint(IPAddress.Any, 0);
+				while(true) {
+					var recv = await _client.ReceiveAsync();
+					var data = Encoding.UTF8.GetString(recv.Buffer);
+					var _    = OnDataReceived(JsonConvert.DeserializeObject<ShardComMessage>(data));
+				}
+			});
+		}
 
-        public void Dispose()
-        {
-            _client.Dispose();
-        }
+		public void Dispose() {
+			_client.Dispose();
+		}
 
-        public event Func<ShardComMessage, Task> OnDataReceived = delegate { return Task.CompletedTask; };
-    }
+		public event Func<ShardComMessage, Task> OnDataReceived = delegate { return Task.CompletedTask; };
+	}
 }
