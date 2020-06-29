8190d671c38b4b03d0e61889ce2319be57c86eed
expeehaa <expeehaa@outlook.com>
2020-06-29 13:39:55 +0200

    Improve blacklist commands and service.

--- src/MitternachtBot/Modules/Permissions/BlacklistCommands.cs
+++ src/MitternachtBot/Modules/Permissions/BlacklistCommands.cs
@@ -2,7 +2,6 @@ using System.Threading.Tasks;
 using Discord;
 using Discord.Commands;
 using Mitternacht.Common.Attributes;
-using Mitternacht.Common.Collections;
 using Mitternacht.Common.TypeReaders;
 using Mitternacht.Modules.Permissions.Services;
 using Mitternacht.Services;
@@ -11,19 +10,11 @@ using Mitternacht.Services.Database.Models;
 namespace Mitternacht.Modules.Permissions {
 	public partial class Permissions {
 		[Group]
-		public class BlacklistCommands : MitternachtSubmodule {
-			private readonly BlacklistService _bs;
+		public class BlacklistCommands : MitternachtSubmodule<BlacklistService> {
 			private readonly DbService _db;
-			private readonly IBotCredentials _creds;
 
-			private ConcurrentHashSet<ulong> BlacklistedUsers => _bs.BlacklistedUsers;
-			private ConcurrentHashSet<ulong> BlacklistedGuilds => _bs.BlacklistedGuilds;
-			private ConcurrentHashSet<ulong> BlacklistedChannels => _bs.BlacklistedChannels;
-
-			public BlacklistCommands(BlacklistService bs, DbService db, IBotCredentials creds) {
-				_bs = bs;
+			public BlacklistCommands(DbService db) {
 				_db = db;
-				_creds = creds;
 			}
 
 			[MitternachtCommand, Usage, Description, Aliases]
@@ -33,13 +24,18 @@ namespace Mitternacht.Modules.Permissions {
 
 			[MitternachtCommand, Usage, Description, Aliases]
 			[OwnerOnly]
-			public Task UserBlacklist(AddRemove action, IUser usr)
-				=> Blacklist(action, usr.Id, BlacklistType.User);
+			public Task UserBlacklist(AddRemove action, IUser user)
+				=> Blacklist(action, user.Id, BlacklistType.User);
 
 			[MitternachtCommand, Usage, Description, Aliases]
 			[OwnerOnly]
 			public Task ChannelBlacklist(AddRemove action, ulong id)
 				=> Blacklist(action, id, BlacklistType.Channel);
+
+			[MitternachtCommand, Usage, Description, Aliases]
+			[OwnerOnly]
+			public Task ChannelBlacklist(AddRemove action, IChannel channel)
+				=> Blacklist(action, channel.Id, BlacklistType.Channel);
 
 			[MitternachtCommand, Usage, Description, Aliases]
 			[OwnerOnly]
@@ -52,37 +48,18 @@ namespace Mitternacht.Modules.Permissions {
 				=> Blacklist(action, guild.Id, BlacklistType.Server);
 
 			private async Task Blacklist(AddRemove action, ulong id, BlacklistType type) {
-				if(action == AddRemove.Add && _creds.OwnerIds.Contains(id))
-					return;
-
-				using(var uow = _db.UnitOfWork) {
-					if(action == AddRemove.Add) {
-						var item = new BlacklistItem { ItemId = id, Type = type };
-						uow.BotConfig.GetOrCreate().Blacklist.Add(item);
-						if(type == BlacklistType.Server) {
-							BlacklistedGuilds.Add(id);
-						} else if(type == BlacklistType.Channel) {
-							BlacklistedChannels.Add(id);
-						} else if(type == BlacklistType.User) {
-							BlacklistedUsers.Add(id);
-						}
-					} else {
-						uow.BotConfig.GetOrCreate().Blacklist.RemoveWhere(bi => bi.ItemId == id && bi.Type == type);
-						if(type == BlacklistType.Server) {
-							BlacklistedGuilds.TryRemove(id);
-						} else if(type == BlacklistType.Channel) {
-							BlacklistedChannels.TryRemove(id);
-						} else if(type == BlacklistType.User) {
-							BlacklistedUsers.TryRemove(id);
-						}
-					}
-					await uow.CompleteAsync().ConfigureAwait(false);
+				using var uow = _db.UnitOfWork;
+				if(action == AddRemove.Add) {
+					uow.BotConfig.GetOrCreate().Blacklist.Add(new BlacklistItem {
+						ItemId = id,
+						Type   = type,
+					});
+				} else {
+					uow.BotConfig.GetOrCreate().Blacklist.RemoveWhere(bi => bi.ItemId == id && bi.Type == type);
 				}
+				await uow.CompleteAsync().ConfigureAwait(false);
 
-				if(action == AddRemove.Add)
-					await ReplyConfirmLocalized("blacklisted", Format.Code(type.ToString()), Format.Code(id.ToString())).ConfigureAwait(false);
-				else
-					await ReplyConfirmLocalized("unblacklisted", Format.Code(type.ToString()), Format.Code(id.ToString())).ConfigureAwait(false);
+				await ReplyConfirmLocalized(action == AddRemove.Add ? "blacklisted" : "unblacklisted", Format.Code(type.ToString()), Format.Code(id.ToString())).ConfigureAwait(false);
 			}
 		}
 	}
