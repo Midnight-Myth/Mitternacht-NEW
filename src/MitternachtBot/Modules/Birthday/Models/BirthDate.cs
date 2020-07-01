b0bad86b46e40a344623671b74abeb513eae382c
expeehaa <expeehaa@outlook.com>
2020-07-01 19:38:29 +0200

    Format files.

--- src/MitternachtBot/Modules/Birthday/Models/BirthDate.cs
+++ src/MitternachtBot/Modules/Birthday/Models/BirthDate.cs
@@ -1,31 +1,28 @@
 using System;
 
-namespace Mitternacht.Modules.Birthday.Models
-{
-    public class BirthDate : IBirthDate
-    {
-        public int Day { get; set; }
-        public int Month { get; set; }
-        public int? Year { get; set; }
+namespace Mitternacht.Modules.Birthday.Models {
+	public class BirthDate : IBirthDate {
+		public int  Day   { get; set; }
+		public int  Month { get; set; }
+		public int? Year  { get; set; }
 
-        public static BirthDate Today => new BirthDate(DateTime.Now.Day, DateTime.Now.Month, DateTime.Now.Year);
-        public static BirthDate TodayWithoutYear => new BirthDate(DateTime.Now.Day, DateTime.Now.Month);
+		public static BirthDate Today            => new BirthDate(DateTime.Now.Day, DateTime.Now.Month, DateTime.Now.Year);
+		public static BirthDate TodayWithoutYear => new BirthDate(DateTime.Now.Day, DateTime.Now.Month);
 
-        public BirthDate(int day, int month, int? year = null)
-        {
-            new DateTime(year ?? 2000, month, day);
-            Day = day;
-            Month = month;
-            Year = year;
-        }
+		public BirthDate(int day, int month, int? year = null) {
+			new DateTime(year ?? 2000, month, day);
+			Day   = day;
+			Month = month;
+			Year  = year;
+		}
 
-        public bool IsBirthday(DateTime date)
-            => date.Day == Day && date.Month == Month;
+		public bool IsBirthday(DateTime date)
+			=> date.Day == Day && date.Month == Month;
 
-        public bool IsBirthday(IBirthDate bd) 
-            => bd.Day == Day && bd.Month == Month;
+		public bool IsBirthday(IBirthDate bd)
+			=> bd.Day == Day && bd.Month == Month;
 
-        public override string ToString()
-            => $"{Day:D2}.{Month:D2}.{(Year.HasValue ? $"{Year.Value:D4}" : string.Empty)}";
-    }
+		public override string ToString()
+			=> $"{Day:D2}.{Month:D2}.{(Year.HasValue ? $"{Year.Value:D4}" : string.Empty)}";
+	}
 }
