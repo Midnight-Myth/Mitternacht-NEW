ece7de1d72871ccf9b4bbd7ad3e983492528346a
expeehaa <expeehaa@outlook.com>
2020-07-01 18:57:45 +0200

    Remove unused extension method IEnumerable<T>#Distinct.

--- src/MitternachtBot/Extensions/EnumerableExtensions.cs
+++ src/MitternachtBot/Extensions/EnumerableExtensions.cs
@@ -1,12 +1,8 @@
-using System;
 using System.Collections.Generic;
 using System.Linq;
 
 namespace Mitternacht.Extensions {
 	public static class EnumerableExtensions {
-		public static IEnumerable<T> Distinct<T, U>(this IEnumerable<T> data, Func<T, U> getKey)
-			=> data.GroupBy(getKey).Select(x => x.First());
-
 		public static T MaxOr<T>(this IEnumerable<T> list, T defaultValue)
 			=> list.Any() ? list.Max() : defaultValue;
 	}
