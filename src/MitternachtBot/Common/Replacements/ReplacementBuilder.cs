100fc7f7487c79c8cc256159f124acc5df0af1be
expeehaa <expeehaa@outlook.com>
2020-07-02 00:06:17 +0200

    Format Replacement files.

--- src/MitternachtBot/Common/Replacements/ReplacementBuilder.cs
+++ src/MitternachtBot/Common/Replacements/ReplacementBuilder.cs
@@ -8,107 +8,93 @@ using Discord.WebSocket;
 using Mitternacht.Extensions;
 using Mitternacht.Modules.Administration.Services;
 
-namespace Mitternacht.Common.Replacements
-{
-    public class ReplacementBuilder
-    {
-        private static readonly Regex RngRegex = new Regex("%rng(?:(?<from>(?:-)?\\d+)-(?<to>(?:-)?\\d+))?%", RegexOptions.Compiled);
-        private readonly ConcurrentDictionary<string, Func<string>> _reps = new ConcurrentDictionary<string, Func<string>>();
-        private readonly ConcurrentDictionary<Regex, Func<Match, string>> _regex = new ConcurrentDictionary<Regex, Func<Match, string>>();
+namespace Mitternacht.Common.Replacements {
+	public class ReplacementBuilder {
+		private static readonly Regex RngRegex = new Regex("%rng(?:(?<from>(?:-)?\\d+)-(?<to>(?:-)?\\d+))?%", RegexOptions.Compiled);
+		private readonly ConcurrentDictionary<string, Func<string>> _reps = new ConcurrentDictionary<string, Func<string>>();
+		private readonly ConcurrentDictionary<Regex, Func<Match, string>> _regex = new ConcurrentDictionary<Regex, Func<Match, string>>();
 
-        public ReplacementBuilder()
-        {
-            WithRngRegex();
-        }
+		public ReplacementBuilder() {
+			WithRngRegex();
+		}
 
-        public ReplacementBuilder WithDefault(IUser usr, IMessageChannel ch, IGuild g, DiscordSocketClient client)
-        {
-            return this.WithUser(usr)
-                .WithChannel(ch)
-                .WithServer(client, g)
-                .WithClient(client);
-        }
+		public ReplacementBuilder WithDefault(IUser usr, IMessageChannel ch, IGuild g, DiscordSocketClient client) {
+			return this.WithUser(usr)
+				.WithChannel(ch)
+				.WithServer(client, g)
+				.WithClient(client);
+		}
 
-        public ReplacementBuilder WithDefault(ICommandContext ctx) =>
-            WithDefault(ctx.User, ctx.Channel, ctx.Guild, (DiscordSocketClient)ctx.Client);
+		public ReplacementBuilder WithDefault(ICommandContext ctx) =>
+			WithDefault(ctx.User, ctx.Channel, ctx.Guild, (DiscordSocketClient)ctx.Client);
 
-        public ReplacementBuilder WithClient(DiscordSocketClient client)
-        {
-            _reps.TryAdd("%mention%", () => $"<@{client.CurrentUser.Id}>");
-            _reps.TryAdd("%shardid%", () => client.ShardId.ToString());
-            _reps.TryAdd("%time%", () => DateTime.Now.ToString("HH:mm " + TimeZoneInfo.Local.StandardName.GetInitials()));
-            return this;
-        }
+		public ReplacementBuilder WithClient(DiscordSocketClient client) {
+			_reps.TryAdd("%mention%", () => $"<@{client.CurrentUser.Id}>");
+			_reps.TryAdd("%shardid%", () => client.ShardId.ToString());
+			_reps.TryAdd("%time%", () => DateTime.Now.ToString("HH:mm " + TimeZoneInfo.Local.StandardName.GetInitials()));
+			return this;
+		}
 
-        public ReplacementBuilder WithServer(DiscordSocketClient client, IGuild g)
-        {
+		public ReplacementBuilder WithServer(DiscordSocketClient client, IGuild g) {
 
-            _reps.TryAdd("%sid%", () => g == null ? "DM" : g.Id.ToString());
-            _reps.TryAdd("%server%", () => g == null ? "DM" : g.Name);
-            _reps.TryAdd("%server_time%", () =>
-            {
-                var to = TimeZoneInfo.Local;
-                if (g == null)
-                    return TimeZoneInfo.ConvertTime(DateTime.UtcNow, TimeZoneInfo.Utc, to).ToString("HH:mm ") + to.StandardName.GetInitials();
-                if (GuildTimezoneService.AllServices.TryGetValue(client.CurrentUser.Id, out var tz))
-                    to = tz.GetTimeZoneOrDefault(g.Id) ?? TimeZoneInfo.Local;
+			_reps.TryAdd("%sid%", () => g == null ? "DM" : g.Id.ToString());
+			_reps.TryAdd("%server%", () => g == null ? "DM" : g.Name);
+			_reps.TryAdd("%server_time%", () => {
+				var to = TimeZoneInfo.Local;
+				if(g == null)
+					return TimeZoneInfo.ConvertTime(DateTime.UtcNow, TimeZoneInfo.Utc, to).ToString("HH:mm ") + to.StandardName.GetInitials();
+				if(GuildTimezoneService.AllServices.TryGetValue(client.CurrentUser.Id, out var tz))
+					to = tz.GetTimeZoneOrDefault(g.Id) ?? TimeZoneInfo.Local;
 
-                return TimeZoneInfo.ConvertTime(DateTime.UtcNow, TimeZoneInfo.Utc, to).ToString("HH:mm ") + to.StandardName.GetInitials();
-            });
-            return this;
-        }
+				return TimeZoneInfo.ConvertTime(DateTime.UtcNow, TimeZoneInfo.Utc, to).ToString("HH:mm ") + to.StandardName.GetInitials();
+			});
+			return this;
+		}
 
-        public ReplacementBuilder WithChannel(IMessageChannel ch)
-        {
-            _reps.TryAdd("%channel%", () => (ch as ITextChannel)?.Mention ?? "#" + ch.Name);
-            _reps.TryAdd("%chname%", () => ch.Name);
-            _reps.TryAdd("%cid%", () => ch?.Id.ToString());
-            return this;
-        }
+		public ReplacementBuilder WithChannel(IMessageChannel ch) {
+			_reps.TryAdd("%channel%", () => (ch as ITextChannel)?.Mention ?? "#" + ch.Name);
+			_reps.TryAdd("%chname%", () => ch.Name);
+			_reps.TryAdd("%cid%", () => ch?.Id.ToString());
+			return this;
+		}
 
-        public ReplacementBuilder WithUser(IUser user)
-        {
-            _reps.TryAdd("%user%", () => user.Mention);
-            _reps.TryAdd("%userfull%", () => Format.Sanitize(user.ToString()));
-            _reps.TryAdd("%username%", () => Format.Sanitize(user.Username));
-            _reps.TryAdd("%userdiscrim%", () => user.Discriminator);
-            _reps.TryAdd("%id%", () => user.Id.ToString());
-            _reps.TryAdd("%uid%", () => user.Id.ToString());
-            return this;
-        }
+		public ReplacementBuilder WithUser(IUser user) {
+			_reps.TryAdd("%user%", () => user.Mention);
+			_reps.TryAdd("%userfull%", () => Format.Sanitize(user.ToString()));
+			_reps.TryAdd("%username%", () => Format.Sanitize(user.Username));
+			_reps.TryAdd("%userdiscrim%", () => user.Discriminator);
+			_reps.TryAdd("%id%", () => user.Id.ToString());
+			_reps.TryAdd("%uid%", () => user.Id.ToString());
+			return this;
+		}
 
-        public ReplacementBuilder WithStats(DiscordSocketClient c)
-        {
-            _reps.TryAdd("%servers%", () => c.Guilds.Count.ToString());
-            _reps.TryAdd("%users%", () => c.Guilds.Sum(s => s.Users.Count).ToString());
-            return this;
-        }
+		public ReplacementBuilder WithStats(DiscordSocketClient c) {
+			_reps.TryAdd("%servers%", () => c.Guilds.Count.ToString());
+			_reps.TryAdd("%users%", () => c.Guilds.Sum(s => s.Users.Count).ToString());
+			return this;
+		}
 
-        public ReplacementBuilder WithRngRegex()
-        {
-            var rng = new NadekoRandom();
-            _regex.TryAdd(RngRegex, match =>
-            {
-                int.TryParse(match.Groups["from"].ToString(), out var from);
-                int.TryParse(match.Groups["to"].ToString(), out var to);
+		public ReplacementBuilder WithRngRegex() {
+			var rng = new NadekoRandom();
+			_regex.TryAdd(RngRegex, match => {
+				int.TryParse(match.Groups["from"].ToString(), out var from);
+				int.TryParse(match.Groups["to"].ToString(), out var to);
 
-                if (from == 0 && to == 0)
-                    return rng.Next(0, 11).ToString();
+				if(from == 0 && to == 0)
+					return rng.Next(0, 11).ToString();
 
-                return from >= to ? string.Empty : rng.Next(from, to + 1).ToString();
-            });
-            return this;
-        }
+				return from >= to ? string.Empty : rng.Next(from, to + 1).ToString();
+			});
+			return this;
+		}
 
-        public ReplacementBuilder WithOverride(string key, Func<string> output)
-        {
-            _reps.AddOrUpdate(key, output, delegate { return output; });
-            return this;
-        }
+		public ReplacementBuilder WithOverride(string key, Func<string> output) {
+			_reps.AddOrUpdate(key, output, delegate { return output; });
+			return this;
+		}
 
-        public Replacer Build()
-        {
-            return new Replacer(_reps.Select(x => (x.Key, x.Value)).ToArray(), _regex.Select(x => (x.Key, x.Value)).ToArray());
-        }
-    }
+		public Replacer Build() {
+			return new Replacer(_reps.Select(x => (x.Key, x.Value)).ToArray(), _regex.Select(x => (x.Key, x.Value)).ToArray());
+		}
+	}
 }
--- src/MitternachtBot/Common/Replacements/Replacer.cs
+++ src/MitternachtBot/Common/Replacements/Replacer.cs
@@ -3,44 +3,38 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text.RegularExpressions;
 
-namespace Mitternacht.Common.Replacements
-{
-    public class Replacer
-    {
-        private readonly IEnumerable<(string Key, Func<string> Text)> _replacements;
-        private readonly IEnumerable<(Regex Regex, Func<Match, string> Replacement)> _regex;
+namespace Mitternacht.Common.Replacements {
+	public class Replacer {
+		private readonly IEnumerable<(string Key, Func<string> Text)> _replacements;
+		private readonly IEnumerable<(Regex Regex, Func<Match, string> Replacement)> _regex;
 
-        public Replacer(IEnumerable<(string, Func<string>)> replacements, IEnumerable<(Regex, Func<Match, string>)> regex)
-        {
-            _replacements = replacements;
-            _regex = regex;
-        }
+		public Replacer(IEnumerable<(string, Func<string>)> replacements, IEnumerable<(Regex, Func<Match, string>)> regex) {
+			_replacements = replacements;
+			_regex = regex;
+		}
 
-        public string Replace(string input)
-        {
-            if (string.IsNullOrWhiteSpace(input))
-                return input;
-            foreach (var (key, text) in _replacements)
-                if (input.Contains(key))
-                    input = input.Replace(key, text());
-            return _regex.Aggregate(input, (current, item) => item.Regex.Replace(current, m => item.Replacement(m)));
-        }
+		public string Replace(string input) {
+			if(string.IsNullOrWhiteSpace(input))
+				return input;
+			foreach(var (key, text) in _replacements)
+				if(input.Contains(key))
+					input = input.Replace(key, text());
+			return _regex.Aggregate(input, (current, item) => item.Regex.Replace(current, m => item.Replacement(m)));
+		}
 
-        public void Replace(CREmbed embedData)
-        {
-            embedData.PlainText = Replace(embedData.PlainText);
-            embedData.Description = Replace(embedData.Description);
-            embedData.Title = Replace(embedData.Title);
+		public void Replace(CREmbed embedData) {
+			embedData.PlainText = Replace(embedData.PlainText);
+			embedData.Description = Replace(embedData.Description);
+			embedData.Title = Replace(embedData.Title);
 
-            if (embedData.Fields != null)
-                foreach (var f in embedData.Fields)
-                {
-                    f.Name = Replace(f.Name);
-                    f.Value = Replace(f.Value);
-                }
+			if(embedData.Fields != null)
+				foreach(var f in embedData.Fields) {
+					f.Name = Replace(f.Name);
+					f.Value = Replace(f.Value);
+				}
 
-            if (embedData.Footer != null)
-                embedData.Footer.Text = Replace(embedData.Footer.Text);
-        }
-    }
+			if(embedData.Footer != null)
+				embedData.Footer.Text = Replace(embedData.Footer.Text);
+		}
+	}
 }
