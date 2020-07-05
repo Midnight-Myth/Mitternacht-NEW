e128488c0aa6acae427853980f2e097bd94a1a12
expeehaa <expeehaa@outlook.com>
2020-07-05 16:40:42 +0200

    Remove caching in CommandMapService.

--- src/MitternachtBot/Modules/Utility/CommandMapCommands.cs
+++ src/MitternachtBot/Modules/Utility/CommandMapCommands.cs
@@ -1,5 +1,3 @@
-using System.Collections.Concurrent;
-using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Discord;
@@ -9,119 +7,70 @@ using Microsoft.EntityFrameworkCore;
 using Mitternacht.Common.Attributes;
 using Mitternacht.Extensions;
 using Mitternacht.Modules.Utility.Services;
-using Mitternacht.Services;
 using Mitternacht.Services.Database;
 using Mitternacht.Services.Database.Models;
 
-namespace Mitternacht.Modules.Utility
-{
-    public partial class Utility
-    {
-        [Group]
-        public class CommandMapCommands : MitternachtSubmodule<CommandMapService>
-        {
-            private readonly IUnitOfWork uow;
-            private readonly DiscordSocketClient _client;
+namespace Mitternacht.Modules.Utility {
+	public partial class Utility {
+		[Group]
+		public class CommandMapCommands : MitternachtSubmodule<CommandMapService> {
+			private readonly IUnitOfWork uow;
+			private readonly DiscordSocketClient _client;
 
-            public CommandMapCommands(IUnitOfWork uow, DiscordSocketClient client)
-            {
-                this.uow = uow;
-                _client = client;
-            }
+			public CommandMapCommands(IUnitOfWork uow, DiscordSocketClient client) {
+				this.uow = uow;
+				_client = client;
+			}
 
-            [MitternachtCommand, Usage, Description, Aliases]
-            [RequireUserPermission(GuildPermission.Administrator)]
-            [RequireContext(ContextType.Guild)]
-            public async Task Alias(string trigger, [Remainder] string mapping = null)
-            {
-                var channel = (ITextChannel)Context.Channel;
+			[MitternachtCommand, Usage, Description, Aliases]
+			[RequireUserPermission(GuildPermission.Administrator)]
+			[RequireContext(ContextType.Guild)]
+			public async Task Alias(string trigger, [Remainder] string mapping = null) {
+				if(string.IsNullOrWhiteSpace(trigger))
+					return;
 
-                if (string.IsNullOrWhiteSpace(trigger))
-                    return;
+				trigger = trigger.Trim();
 
-                trigger = trigger.Trim().ToLowerInvariant();
+				var gc = uow.GuildConfigs.For(Context.Guild.Id, set => set.Include(x => x.CommandAliases));
 
-                if (string.IsNullOrWhiteSpace(mapping))
-                {
-                    if (!Service.AliasMaps.TryGetValue(Context.Guild.Id, out var maps) ||
-                        !maps.TryRemove(trigger, out _))
-                    {
-                        await ReplyErrorLocalized("alias_remove_fail", Format.Code(trigger)).ConfigureAwait(false);
-                        return;
-                    }
+				if(string.IsNullOrWhiteSpace(mapping)) {
+					gc.CommandAliases.RemoveWhere(x => x.Trigger == trigger);
+					uow.SaveChanges(false);
 
-                    var config = uow.GuildConfigs.For(Context.Guild.Id, set => set.Include(x => x.CommandAliases));
-                    var toAdd = new CommandAlias()
-                    {
-                        Mapping = mapping,
-                        Trigger = trigger
-                    };
-                    config.CommandAliases.RemoveWhere(x => x.Trigger == trigger);
-                    uow.SaveChanges(false);
+					await ReplyConfirmLocalized("alias_removed", Format.Code(trigger)).ConfigureAwait(false);
+				} else {
+					gc.CommandAliases.RemoveWhere(x => x.Trigger == trigger);
+					gc.CommandAliases.Add(new CommandAlias {
+						Mapping = mapping,
+						Trigger = trigger,
+					});
+					uow.SaveChanges(false);
 
-                    await ReplyConfirmLocalized("alias_removed", Format.Code(trigger)).ConfigureAwait(false);
-                    return;
-                }
-                Service.AliasMaps.AddOrUpdate(Context.Guild.Id, (_) =>
-                {
-                    var config = uow.GuildConfigs.For(Context.Guild.Id, set => set.Include(x => x.CommandAliases));
-                    config.CommandAliases.Add(new CommandAlias()
-                    {
-                        Mapping = mapping,
-                        Trigger = trigger
-                    });
-                    uow.SaveChanges(false);
-
-                    return new ConcurrentDictionary<string, string>(new Dictionary<string, string>() {
-                        {trigger.Trim().ToLowerInvariant(), mapping.ToLowerInvariant() },
-                    });
-                }, (_, map) =>
-                {
-                    var config = uow.GuildConfigs.For(Context.Guild.Id, set => set.Include(x => x.CommandAliases));
-                    var toAdd = new CommandAlias()
-                    {
-                        Mapping = mapping,
-                        Trigger = trigger
-                    };
-                    config.CommandAliases.RemoveWhere(x => x.Trigger == trigger);
-                    config.CommandAliases.Add(toAdd);
-                    uow.SaveChanges(false);
-
-                    map.AddOrUpdate(trigger, mapping, (key, old) => mapping);
-                    return map;
-                });
-
-                await ReplyConfirmLocalized("alias_added", Format.Code(trigger), Format.Code(mapping)).ConfigureAwait(false);
-            }
+					await ReplyConfirmLocalized("alias_added", Format.Code(trigger), Format.Code(mapping)).ConfigureAwait(false);
+				}
+			}
 
 
-            [MitternachtCommand, Usage, Description, Aliases]
-            [RequireContext(ContextType.Guild)]
-            public async Task AliasList(int page = 1)
-            {
-                var channel = (ITextChannel)Context.Channel;
-                page -= 1;
+			[MitternachtCommand, Usage, Description, Aliases]
+			[RequireContext(ContextType.Guild)]
+			public async Task AliasList(int page = 1) {
+				page -= 1;
 
-                if (page < 0)
-                    return;
+				if(page < 0)
+					return;
 
-                if (!Service.AliasMaps.TryGetValue(Context.Guild.Id, out var maps) || !maps.Any())
-                {
-                    await ReplyErrorLocalized("aliases_none").ConfigureAwait(false);
-                    return;
-                }
+				var commandAliases = uow.GuildConfigs.For(Context.Guild.Id, set => set.Include(x => x.CommandAliases)).CommandAliases.ToArray();
 
-                var arr = maps.ToArray();
-
-                await Context.Channel.SendPaginatedConfirmAsync(_client, page, curPage =>
-                {
-                    return new EmbedBuilder().WithOkColor()
-                    .WithTitle(GetText("alias_list"))
-                    .WithDescription(string.Join("\n",
-                        arr.Skip(curPage * 10).Take(10).Select(x => $"`{x.Key}` => `{x.Value}`")));
-
-                }, arr.Length / 10, reactUsers: new[] { Context.User as IGuildUser }).ConfigureAwait(false);
-            }
-        }
-    }
+				if(commandAliases.Any()) {
+					await Context.Channel.SendPaginatedConfirmAsync(_client, page, curPage => {
+						return new EmbedBuilder().WithOkColor()
+							.WithTitle(GetText("alias_list"))
+							.WithDescription(string.Join("\n", commandAliases.Skip(curPage * 10).Take(10).Select(x => $"`{x.Trigger}` => `{x.Mapping}`")));
+					}, commandAliases.Length / 10, reactUsers: new[] { Context.User as IGuildUser }).ConfigureAwait(false);
+				} else {
+					await ReplyErrorLocalized("aliases_none").ConfigureAwait(false);
+				}
+			}
+		}
+	}
 }
--- src/MitternachtBot/Modules/Utility/Services/CommandMapService.cs
+++ src/MitternachtBot/Modules/Utility/Services/CommandMapService.cs
@@ -1,66 +1,64 @@
-using System.Collections.Concurrent;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Discord;
+using Microsoft.EntityFrameworkCore;
 using Mitternacht.Common.ModuleBehaviors;
 using Mitternacht.Extensions;
 using Mitternacht.Services;
 using Mitternacht.Services.Database.Models;
 using NLog;
 
-namespace Mitternacht.Modules.Utility.Services
-{
-    public class CommandMapService : IInputTransformer, IMService
-    {
-        private readonly Logger _log;
+namespace Mitternacht.Modules.Utility.Services {
+	public class CommandMapService : IInputTransformer, IMService {
+		private readonly Logger _log;
+		private readonly DbService _db;
 
-        public ConcurrentDictionary<ulong, ConcurrentDictionary<string, string>> AliasMaps { get; }
-        //commandmap
-        public CommandMapService(IEnumerable<GuildConfig> gcs)
-        {
-            _log = LogManager.GetCurrentClassLogger();
-            AliasMaps = new ConcurrentDictionary<ulong, ConcurrentDictionary<string, string>>(
-                gcs.ToDictionary(
-                    x => x.GuildId,
-                        x => new ConcurrentDictionary<string, string>(x.CommandAliases
-                            .Distinct(new CommandAliasEqualityComparer())
-                            .ToDictionary(ca => ca.Trigger, ca => ca.Mapping))));
-        }
+		public CommandMapService(DbService db) {
+			_log = LogManager.GetCurrentClassLogger();
+			_db = db;
+		}
 
-        public async Task<string> TransformInput(IGuild guild, IMessageChannel channel, IUser user, string input, bool realExecution = true)
-        {
-            await Task.Yield();
+		public async Task<string> TransformInput(IGuild guild, IMessageChannel channel, IUser user, string input, bool realExecution = true) {
+			await Task.Yield();
 
-            if (guild == null || string.IsNullOrWhiteSpace(input))
-                return input;
-            
-            input = input.ToLowerInvariant();
-            if (!AliasMaps.TryGetValue(guild.Id, out var maps)) return input;
-            var keys = maps.Keys.OrderByDescending(x => x.Length);
+			if(guild == null || string.IsNullOrWhiteSpace(input))
+				return input;
 
-            foreach (var k in keys)
-            {
-                string newInput;
-                if (input.StartsWith(k + " ")) newInput = maps[k] + input.Substring(k.Length, input.Length - k.Length);
-                else if (input == k) newInput = maps[k];
-                else continue;
+			input = input.ToLowerInvariant();
 
-                if (!realExecution) return newInput;
+			using var uow = _db.UnitOfWork;
+			var commandAliases = uow.GuildConfigs.For(guild.Id, set => set.Include(x => x.CommandAliases)).CommandAliases.Distinct(new CommandAliasEqualityComparer());
 
-                _log.Info($"--Mapping Command--\nGuildId: {guild.Id}\nTrigger: {input}\nMapping: {newInput}");
-                try { await channel.SendConfirmAsync($"{input} => {newInput}").ConfigureAwait(false); } catch { /*ignore*/ }
-                return newInput;
-            }
+			if(!commandAliases.Any())
+				return input;
 
-            return input;
-        }
-    }
+			foreach(var ca in commandAliases.OrderByDescending(ca => ca.Trigger.Length)) {
+				string newInput;
+				if(input.StartsWith($"{ca.Trigger} ", StringComparison.OrdinalIgnoreCase))
+					newInput = $"{ca.Mapping}{input[ca.Trigger.Length..]}";
+				else if(input.Equals(ca.Trigger, StringComparison.OrdinalIgnoreCase))
+					newInput = ca.Mapping;
+				else
+					continue;
 
-    public class CommandAliasEqualityComparer : IEqualityComparer<CommandAlias>
-    {
-        public bool Equals(CommandAlias x, CommandAlias y) => x.Trigger == y.Trigger;
+				if(!realExecution)
+					return newInput;
 
-        public int GetHashCode(CommandAlias obj) => obj.Trigger.GetHashCode();
-    }
+				_log.Info($"--Mapping Command--\nGuildId: {guild.Id}\nTrigger: {input}\nMapping: {newInput}");
+				try { await channel.SendConfirmAsync($"{input} => {newInput}").ConfigureAwait(false); } catch { }
+				return newInput;
+			}
+
+			return input;
+		}
+	}
+
+	public class CommandAliasEqualityComparer : IEqualityComparer<CommandAlias> {
+		public bool Equals(CommandAlias x, CommandAlias y)
+			=> x.Trigger == y.Trigger;
+
+		public int GetHashCode(CommandAlias obj) => obj.Trigger.GetHashCode();
+	}
 }
