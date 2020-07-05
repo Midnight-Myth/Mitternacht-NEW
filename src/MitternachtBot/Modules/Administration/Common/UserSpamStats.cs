a2384da1dc7546617736dae04bed5f2498a5fa5b
expeehaa <expeehaa@outlook.com>
2020-07-05 19:49:13 +0200

    Remove database caching from ProtectionService.

--- src/MitternachtBot/Modules/Administration/Common/UserSpamStats.cs
+++ src/MitternachtBot/Modules/Administration/Common/UserSpamStats.cs
@@ -4,47 +4,47 @@ using System.Linq;
 using System.Threading;
 using Discord;
 
-namespace Mitternacht.Modules.Administration.Common
-{
-    public class UserSpamStats : IDisposable
-    {
-        public int Count => timers.Count;
-        public string LastMessage { get; set; }
+namespace Mitternacht.Modules.Administration.Common {
+	public class UserSpamStats : IDisposable {
+		public int Count => _timers.Count;
+		public string LastMessage { get; set; }
 
-        private ConcurrentQueue<Timer> timers { get; }
+		private readonly ConcurrentQueue<Timer> _timers = new ConcurrentQueue<Timer>();
 
-        public UserSpamStats(IUserMessage msg)
-        {
-            LastMessage = msg.Content.ToUpperInvariant();
-            timers = new ConcurrentQueue<Timer>();
+		public UserSpamStats(IUserMessage msg) {
+			LastMessage = msg.Content;
 
-            ApplyNextMessage(msg);
-        }
+			ApplyNextMessage(msg);
+		}
 
-        private readonly object applyLock = new object();
-        public void ApplyNextMessage(IUserMessage message)
-        {
-            lock (applyLock)
-            {
-                var upperMsg = message.Content.ToUpperInvariant();
-                if (upperMsg != LastMessage || (string.IsNullOrWhiteSpace(upperMsg) && message.Attachments.Any()))
-                {
-                    LastMessage = upperMsg;
-                    while (timers.TryDequeue(out var old))
-                        old.Change(Timeout.Infinite, Timeout.Infinite);
-                }
-                var t = new Timer((_) => {
-                    if (timers.TryDequeue(out var old))
-                        old.Change(Timeout.Infinite, Timeout.Infinite);
-                }, null, TimeSpan.FromMinutes(30), TimeSpan.FromMinutes(30));
-                timers.Enqueue(t);
-            }
-        }
+		private readonly object applyLock = new object();
 
-        public void Dispose()
-        {
-            while (timers.TryDequeue(out var old))
-                old.Change(Timeout.Infinite, Timeout.Infinite);
-        }
-    }
+		public void ApplyNextMessage(IUserMessage message) {
+			lock(applyLock) {
+				if(!message.Content.Equals(LastMessage, StringComparison.OrdinalIgnoreCase) || string.IsNullOrWhiteSpace(message.Content) && message.Attachments.Any()) {
+					LastMessage = message.Content;
+					while(_timers.TryDequeue(out var old)) {
+						StopAllTimers();
+					}
+				}
+
+				var t = new Timer((_) => {
+					if(_timers.TryDequeue(out var old))
+						old.Dispose();
+				}, null, TimeSpan.FromMinutes(30), TimeSpan.FromMinutes(30));
+
+				_timers.Enqueue(t);
+			}
+		}
+
+		private void StopAllTimers() {
+			while(_timers.TryDequeue(out var old)) {
+				old.Dispose();
+			}
+		}
+
+		public void Dispose() {
+			StopAllTimers();
+		}
+	}
 }
