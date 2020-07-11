e4b44b259901db6b1dad9d35aab85e19e0592f09
expeehaa <expeehaa@outlook.com>
2020-07-11 15:48:50 +0200

    Improve DailyMoneyRepository.

--- src/MitternachtBot/Services/Database/Repositories/Impl/DailyMoneyRepository.cs
+++ src/MitternachtBot/Services/Database/Repositories/Impl/DailyMoneyRepository.cs
@@ -3,49 +3,49 @@ using System.Linq;
 using Microsoft.EntityFrameworkCore;
 using Mitternacht.Services.Database.Models;
 
-namespace Mitternacht.Services.Database.Repositories.Impl
-{
-    public class DailyMoneyRepository : Repository<DailyMoney>, IDailyMoneyRepository
-    {
-        public DailyMoneyRepository(DbContext context) : base(context)
-        {
-        }
+namespace Mitternacht.Services.Database.Repositories.Impl {
+	public class DailyMoneyRepository : Repository<DailyMoney>, IDailyMoneyRepository {
+		public DailyMoneyRepository(DbContext context) : base(context) { }
 
-        public DailyMoney GetOrCreate(ulong userId)
-        {
-            var cur = _set.FirstOrDefault(c => c.UserId == userId);
+		public DailyMoney GetOrCreate(ulong userId) {
+			var cur = _set.FirstOrDefault(c => c.UserId == userId);
 
-            if (cur != null) return cur;
-            _set.Add(cur = new DailyMoney
-            {
-                UserId = userId,
-                LastTimeGotten = DateTime.MinValue
-            });
-            return cur;
-        }
+			if(cur == null) {
+				_set.Add(cur = new DailyMoney {
+					UserId = userId,
+					LastTimeGotten = DateTime.MinValue
+				});
+			}
 
-        public DateTime GetUserDate(ulong userId) 
-            => GetOrCreate(userId).LastTimeGotten;
+			return cur;
+		}
 
-        public bool CanReceive(ulong userId) 
-            => GetOrCreate(userId).LastTimeGotten.Date < DateTime.Today.Date;
+		public DateTime GetUserDate(ulong userId)
+			=> GetOrCreate(userId).LastTimeGotten;
 
-        public bool TryUpdateState(ulong userId)
-        {
-            var dm = GetOrCreate(userId);
-            if (dm.LastTimeGotten.Date >= DateTime.Today.Date) return false;
-            dm.LastTimeGotten = DateTime.Now;
-            _set.Update(dm);
-            return true;
-        }
+		public bool CanReceive(ulong userId)
+			=> GetOrCreate(userId).LastTimeGotten.Date < DateTime.Today.Date;
 
-        public bool TryResetReceived(ulong userId)
-        {
-            var dm = GetOrCreate(userId);
-            if (dm.LastTimeGotten.Date < DateTime.Today.Date) return false;
-            dm.LastTimeGotten = DateTime.Today.AddDays(-1);
-            _set.Update(dm);
-            return true;
-        }
-    }
+		public bool TryUpdateState(ulong userId) {
+			var dm = GetOrCreate(userId);
+			
+			if(dm.LastTimeGotten.Date < DateTime.Today.Date) {
+				dm.LastTimeGotten = DateTime.Now;
+				return true;
+			} else {
+				return false;
+			}
+		}
+
+		public bool TryResetReceived(ulong userId) {
+			var dm = GetOrCreate(userId);
+
+			if(dm.LastTimeGotten.Date >= DateTime.Today.Date) {
+				dm.LastTimeGotten = DateTime.Today.AddDays(-1);
+				return true;
+			} else {
+				return false;
+			}
+		}
+	}
 }
