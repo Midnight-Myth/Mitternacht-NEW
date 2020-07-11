30d9cd0099c1364c2ce3b0ab79ded4cc574dd4a8
expeehaa <expeehaa@outlook.com>
2020-07-11 17:04:52 +0200

    Improve RoleLevelBindingRepository.

--- src/MitternachtBot/Services/Database/Repositories/IRoleLevelBindingRepository.cs
+++ src/MitternachtBot/Services/Database/Repositories/IRoleLevelBindingRepository.cs
@@ -4,6 +4,5 @@ namespace Mitternacht.Services.Database.Repositories {
 	public interface IRoleLevelBindingRepository : IRepository<RoleLevelBinding> {
 		void SetBinding(ulong roleid, int level);
 		bool Remove(ulong roleid);
-		int GetMinimumLevel(ulong roleid);
 	}
 }
