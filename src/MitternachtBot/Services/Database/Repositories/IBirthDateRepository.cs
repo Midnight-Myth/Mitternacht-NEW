968174446d2b68d88a3c2360a48cc6338000b2c4
expeehaa <expeehaa@outlook.com>
2020-07-11 16:04:39 +0200

    Improve BirthDateRepository.

--- src/MitternachtBot/Services/Database/Repositories/IBirthDateRepository.cs
+++ src/MitternachtBot/Services/Database/Repositories/IBirthDateRepository.cs
@@ -1,12 +1,12 @@
 using System;
-using System.Collections.Generic;
+using System.Linq;
 using Mitternacht.Modules.Birthday.Models;
 using Mitternacht.Services.Database.Models;
 
 namespace Mitternacht.Services.Database.Repositories {
 	public interface IBirthDateRepository : IRepository<BirthDateModel> {
-		IEnumerable<BirthDateModel> GetBirthdays(DateTime date);
-		IEnumerable<BirthDateModel> GetBirthdays(IBirthDate bd, bool checkYear = false);
+		IQueryable<BirthDateModel> GetBirthdays(DateTime date);
+		IQueryable<BirthDateModel> GetBirthdays(IBirthDate bd, bool checkYear = false);
 		BirthDateModel GetUserBirthDate(ulong userid);
 		bool HasBirthDate(ulong userid);
 		void SetBirthDate(ulong userid, IBirthDate bd);
--- src/MitternachtBot/Services/Database/Repositories/Impl/BirthDateRepository.cs
+++ src/MitternachtBot/Services/Database/Repositories/Impl/BirthDateRepository.cs
@@ -1,47 +1,47 @@
 using System;
-using System.Collections.Generic;
 using System.Linq;
 using Microsoft.EntityFrameworkCore;
 using Mitternacht.Modules.Birthday.Models;
 using Mitternacht.Services.Database.Models;
-using System.Linq.Expressions;
 
-namespace Mitternacht.Services.Database.Repositories.Impl
-{
-    public class BirthDateRepository : Repository<BirthDateModel>, IBirthDateRepository
-    {
-        public BirthDateRepository(DbContext context) : base(context) { }
+namespace Mitternacht.Services.Database.Repositories.Impl {
+	public class BirthDateRepository : Repository<BirthDateModel>, IBirthDateRepository {
+		public BirthDateRepository(DbContext context) : base(context) { }
 
-        public IEnumerable<BirthDateModel> GetBirthdays(DateTime date)
-            => _set.Where((Expression<Func<BirthDateModel, bool>>) (b => b.Day == date.Day && b.Month == date.Month)).ToList();
+		public IQueryable<BirthDateModel> GetBirthdays(DateTime date)
+			=> _set.AsQueryable().Where(b => b.Day == date.Day && b.Month == date.Month);
 
-        public IEnumerable<BirthDateModel> GetBirthdays(IBirthDate bd, bool checkYear = false)
-            => _set.Where((Expression<Func<BirthDateModel, bool>>) (b => b.Day == bd.Day && b.Month == bd.Month && (!checkYear || !bd.Year.HasValue || b.Year.HasValue && bd.Year.HasValue && b.Year == bd.Year))).ToList();
+		public IQueryable<BirthDateModel> GetBirthdays(IBirthDate bd, bool checkYear = false)
+			=> _set.AsQueryable().Where(b => b.Day == bd.Day && b.Month == bd.Month && (!checkYear || !bd.Year.HasValue || b.Year.HasValue && bd.Year.HasValue && b.Year == bd.Year));
 
-        public BirthDateModel GetUserBirthDate(ulong userid)
-            => _set.FirstOrDefault(b => b.UserId == userid);
+		public BirthDateModel GetUserBirthDate(ulong userid)
+			=> _set.FirstOrDefault(b => b.UserId == userid);
 
-        public bool HasBirthDate(ulong userid)
-            => _set.Any(b => b.UserId == userid);
+		public bool HasBirthDate(ulong userid)
+			=> _set.Any(b => b.UserId == userid);
 
-        public void SetBirthDate(ulong userid, IBirthDate bd) {
-            var bdm = GetUserBirthDate(userid);
-            if (bdm == null)
-                _set.Add(new BirthDateModel(userid, bd));
-            else {
-                bdm.Update(bd);
-                _set.Update(bdm);
-            }
-        }
+		public void SetBirthDate(ulong userid, IBirthDate bd) {
+			var bdm = GetUserBirthDate(userid);
 
-        public bool DeleteBirthDate(ulong userid) {
-            var bdm = GetUserBirthDate(userid);
-            if (bdm == null) return false;
-            _set.Remove(bdm);
-            return true;
-        }
+			if(bdm == null) {
+				_set.Add(new BirthDateModel(userid, bd));
+			} else {
+				bdm.Update(bd);
+			}
+		}
 
-        public bool? BirthdayMessageEnabled(ulong userId)
-            => GetUserBirthDate(userId)?.BirthdayMessageEnabled;
-    }
+		public bool DeleteBirthDate(ulong userid) {
+			var bdm = GetUserBirthDate(userid);
+			
+			if(bdm != null) {
+				_set.Remove(bdm);
+				return true;
+			} else {
+				return false;
+			}
+		}
+
+		public bool? BirthdayMessageEnabled(ulong userId)
+			=> GetUserBirthDate(userId)?.BirthdayMessageEnabled;
+	}
 }
