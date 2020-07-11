b44b5094f6086eb91e30361f917da5195ccd6e80
expeehaa <expeehaa@outlook.com>
2020-07-11 16:12:58 +0200

    Format CurrencyTransactionsRepository and CustomReactionRepository.

--- src/MitternachtBot/Services/Database/Repositories/Impl/CurrencyTransactionsRepository.cs
+++ src/MitternachtBot/Services/Database/Repositories/Impl/CurrencyTransactionsRepository.cs
@@ -1,12 +1,9 @@
 using Microsoft.EntityFrameworkCore;
 using Mitternacht.Services.Database.Models;
 
-namespace Mitternacht.Services.Database.Repositories.Impl
-{
-    public class CurrencyTransactionsRepository : Repository<CurrencyTransaction>, ICurrencyTransactionsRepository
-    {
-        public CurrencyTransactionsRepository(DbContext context) : base(context)
-        {
-        }
-    }
+namespace Mitternacht.Services.Database.Repositories.Impl {
+	public class CurrencyTransactionsRepository : Repository<CurrencyTransaction>, ICurrencyTransactionsRepository {
+		public CurrencyTransactionsRepository(DbContext context) : base(context) {
+		}
+	}
 }
--- src/MitternachtBot/Services/Database/Repositories/Impl/CustomReactionRepository.cs
+++ src/MitternachtBot/Services/Database/Repositories/Impl/CustomReactionRepository.cs
@@ -1,12 +1,9 @@
 using Microsoft.EntityFrameworkCore;
 using Mitternacht.Services.Database.Models;
 
-namespace Mitternacht.Services.Database.Repositories.Impl
-{
-    public class CustomReactionsRepository : Repository<CustomReaction>, ICustomReactionRepository
-    {
-        public CustomReactionsRepository(DbContext context) : base(context)
-        {
-        }
-    }
+namespace Mitternacht.Services.Database.Repositories.Impl {
+	public class CustomReactionsRepository : Repository<CustomReaction>, ICustomReactionRepository {
+		public CustomReactionsRepository(DbContext context) : base(context) {
+		}
+	}
 }
