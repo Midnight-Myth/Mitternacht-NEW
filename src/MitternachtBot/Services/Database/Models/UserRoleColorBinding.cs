d3e279c8b92e0d44d7689ad9cfcf8322dafab714
expeehaa <expeehaa@outlook.com>
2020-08-15 20:10:02 +0200

    Add UserRoleColorBinding model with repository and migration.

---
+++ src/MitternachtBot/Services/Database/Models/UserRoleColorBinding.cs
@@ -0,0 +1,7 @@
+﻿namespace Mitternacht.Services.Database.Models {
+	public class UserRoleColorBinding : DbEntity {
+		public ulong UserId  { get; set; }
+		public ulong GuildId { get; set; }
+		public ulong RoleId  { get; set; }
+	}
+}
