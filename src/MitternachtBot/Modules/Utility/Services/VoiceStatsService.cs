e39bbeff06dfc7fd20253208c29da48578b837dd
expeehaa <expeehaa@outlook.com>
2020-08-15 14:34:28 +0200

    Format files.

--- src/MitternachtBot/Modules/Utility/Services/VoiceStatsService.cs
+++ src/MitternachtBot/Modules/Utility/Services/VoiceStatsService.cs
@@ -4,79 +4,66 @@ using Mitternacht.Services;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Mitternacht.Modules.Utility.Services
-{
-    public class VoiceStatsService : IMService
-    {
-        private readonly DbService _db;
-        private readonly DiscordSocketClient _client;
-        private Task _writeStats;
-        private readonly VoiceStateTimeHelper _timeHelper;
+namespace Mitternacht.Modules.Utility.Services {
+	public class VoiceStatsService : IMService {
+		private readonly DbService _db;
+		private readonly DiscordSocketClient _client;
+		private Task _writeStats;
+		private readonly VoiceStateTimeHelper _timeHelper;
 
-        public VoiceStatsService(DiscordSocketClient client, DbService db)
-        {
-            _client = client;
-            _db = db;
+		public VoiceStatsService(DiscordSocketClient client, DbService db) {
+			_client = client;
+			_db = db;
 
-            _timeHelper = new VoiceStateTimeHelper();
-            _timeHelper.Reset();
+			_timeHelper = new VoiceStateTimeHelper();
+			_timeHelper.Reset();
 
-            var guildusers = client.Guilds.SelectMany(g => g.VoiceChannels.SelectMany(svc => svc.Users).Select(sgu => (UserId: sgu.Id, GuildId: g.Id))).ToList();
-            foreach ((ulong UserId, ulong GuildId) in guildusers)
-            {
-                _timeHelper.StartTracking(UserId, GuildId);
-            }
+			var guildusers = client.Guilds.SelectMany(g => g.VoiceChannels.SelectMany(svc => svc.Users).Select(sgu => (UserId: sgu.Id, GuildId: g.Id))).ToList();
+			foreach((ulong UserId, ulong GuildId) in guildusers) {
+				_timeHelper.StartTracking(UserId, GuildId);
+			}
 
-            _client.UserVoiceStateUpdated += UserVoiceStateUpdated;
-            _client.JoinedGuild += ClientJoinedGuild;
-            _client.LeftGuild += ClientLeftGuild;
+			_client.UserVoiceStateUpdated += UserVoiceStateUpdated;
+			_client.JoinedGuild += ClientJoinedGuild;
+			_client.LeftGuild += ClientLeftGuild;
 
-            _writeStats = Task.Run(async () =>
-            {
-                while (true)
-                {
-                    try
-                    {
-                        var usertimes = _timeHelper.GetUserTimes();
-                        using (var uow = _db.UnitOfWork)
-                        {
-                            foreach (var ut in usertimes)
-                            {
-                                uow.VoiceChannelStats.AddTime(ut.Key.GuildId, ut.Key.UserId, ut.Value);
-                            }
-                            await uow.SaveChangesAsync().ConfigureAwait(false);
-                        }
-                    }
-                    catch { /* ignored */ }
-                    
-                    await Task.Delay(5000);
-                }
-            });
-        }
+			_writeStats = Task.Run(async () => {
+				while(true) {
+					try {
+						var usertimes = _timeHelper.GetUserTimes();
+						using(var uow = _db.UnitOfWork) {
+							foreach(var ut in usertimes) {
+								uow.VoiceChannelStats.AddTime(ut.Key.GuildId, ut.Key.UserId, ut.Value);
+							}
+							await uow.SaveChangesAsync().ConfigureAwait(false);
+						}
+					} catch { /* ignored */ }
 
-        private Task UserVoiceStateUpdated(SocketUser user, SocketVoiceState stateo, SocketVoiceState staten)
-        {
-            if (stateo.VoiceChannel == null && staten.VoiceChannel != null) _timeHelper.StartTracking(user.Id, staten.VoiceChannel.Guild.Id);
-            if (stateo.VoiceChannel != null && staten.VoiceChannel == null && !_timeHelper.StopTracking(user.Id, stateo.VoiceChannel.Guild.Id))
-                    _timeHelper.EndUserTrackingAfterInterval.Add((user.Id, stateo.VoiceChannel.Guild.Id));
+					await Task.Delay(5000);
+				}
+			});
+		}
 
-            return Task.CompletedTask;
-        }
+		private Task UserVoiceStateUpdated(SocketUser user, SocketVoiceState stateo, SocketVoiceState staten) {
+			if(stateo.VoiceChannel == null && staten.VoiceChannel != null)
+				_timeHelper.StartTracking(user.Id, staten.VoiceChannel.Guild.Id);
+			if(stateo.VoiceChannel != null && staten.VoiceChannel == null && !_timeHelper.StopTracking(user.Id, stateo.VoiceChannel.Guild.Id))
+				_timeHelper.EndUserTrackingAfterInterval.Add((user.Id, stateo.VoiceChannel.Guild.Id));
 
-        private Task ClientJoinedGuild(SocketGuild guild)
-        {
-            var gus = guild.VoiceChannels.SelectMany(svc => svc.Users).Select(sgu => (UserId: sgu.Id, GuildId: guild.Id)).ToList();
-            foreach ((ulong UserId, ulong GuildId) in gus)
-            {
-                _timeHelper.StartTracking(UserId, GuildId);
-            }
-            return Task.CompletedTask;
-        }
+			return Task.CompletedTask;
+		}
 
-        private Task ClientLeftGuild(SocketGuild guild)
-        {
-            _timeHelper.StopGuildTracking(guild.Id);
-            return Task.CompletedTask;
-        }
-    }
+		private Task ClientJoinedGuild(SocketGuild guild) {
+			var gus = guild.VoiceChannels.SelectMany(svc => svc.Users).Select(sgu => (UserId: sgu.Id, GuildId: guild.Id)).ToList();
+			foreach((ulong UserId, ulong GuildId) in gus) {
+				_timeHelper.StartTracking(UserId, GuildId);
+			}
+			return Task.CompletedTask;
+		}
+
+		private Task ClientLeftGuild(SocketGuild guild) {
+			_timeHelper.StopGuildTracking(guild.Id);
+			return Task.CompletedTask;
+		}
+	}
 }
--- src/MitternachtBot/Modules/Utility/VoiceStatsCommands.cs
+++ src/MitternachtBot/Modules/Utility/VoiceStatsCommands.cs
@@ -7,41 +7,37 @@ using System;
 using System.Threading.Tasks;
 
 namespace Mitternacht.Modules.Utility {
-	public partial class Utility
-    {
-        public class VoiceStatsCommands : MitternachtSubmodule<VoiceStatsService>
-        {
-            private readonly IUnitOfWork uow;
+	public partial class Utility {
+		public class VoiceStatsCommands : MitternachtSubmodule<VoiceStatsService> {
+			private readonly IUnitOfWork uow;
 
-            public VoiceStatsCommands(IUnitOfWork uow)
-            {
-                this.uow = uow;
-            }
+			public VoiceStatsCommands(IUnitOfWork uow) {
+				this.uow = uow;
+			}
 
-            [MitternachtCommand, Description, Usage, Aliases]
-            [RequireContext(ContextType.Guild)]
-            public async Task VoiceStats(IGuildUser user = null)
-            {
-                user = user ?? Context.User as IGuildUser;
+			[MitternachtCommand, Description, Usage, Aliases]
+			[RequireContext(ContextType.Guild)]
+			public async Task VoiceStats(IGuildUser user = null) {
+				user = user ?? Context.User as IGuildUser;
 
-				if (uow.VoiceChannelStats.TryGetTime(user.GuildId, user.Id, out var time)) {
+				if(uow.VoiceChannelStats.TryGetTime(user.GuildId, user.Id, out var time)) {
 					var timespan = TimeSpan.FromSeconds(time);
 					await ConfirmLocalized("voicestats_time", user.ToString(), $"{(timespan.Days > 0 ? $"{timespan:dd}d" : "")}{(timespan.Hours > 0 ? $"{timespan:hh}h" : "")}{(timespan.Minutes > 0 ? $"{timespan:mm}min" : "")}{timespan:ss}s").ConfigureAwait(false);
 				} else
-                    await ConfirmLocalized("voicestats_untracked", user.ToString()).ConfigureAwait(false);
-            }
+					await ConfirmLocalized("voicestats_untracked", user.ToString()).ConfigureAwait(false);
+			}
 
-            [MitternachtCommand, Description, Usage, Aliases]
-            [RequireContext(ContextType.Guild)]
-            [OwnerOrGuildPermission(GuildPermission.Administrator)]
-            public async Task VoiceStatsReset(IGuildUser user)
-            {
-                if (user == null) return;
-                uow.VoiceChannelStats.Reset(user.GuildId, user.Id);
-                await uow.SaveChangesAsync(false).ConfigureAwait(false);
+			[MitternachtCommand, Description, Usage, Aliases]
+			[RequireContext(ContextType.Guild)]
+			[OwnerOrGuildPermission(GuildPermission.Administrator)]
+			public async Task VoiceStatsReset(IGuildUser user) {
+				if(user == null)
+					return;
+				uow.VoiceChannelStats.Reset(user.GuildId, user.Id);
+				await uow.SaveChangesAsync(false).ConfigureAwait(false);
 
-                await ConfirmLocalized("voicestats_reset", user.ToString()).ConfigureAwait(false);
-            }
-        }
-    }
+				await ConfirmLocalized("voicestats_reset", user.ToString()).ConfigureAwait(false);
+			}
+		}
+	}
 }
