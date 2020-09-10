677727421ad8871a8e23bd7207ce9d9c1b0e7355
expeehaa <expeehaa@outlook.com>
2020-09-10 16:23:54 +0200

    Remove caching of CountToNumber channels.

--- src/MitternachtBot/Modules/Utility/CountToNumberCommands.cs
+++ src/MitternachtBot/Modules/Utility/CountToNumberCommands.cs
@@ -4,22 +4,31 @@ using Discord;
 using Discord.Commands;
 using Mitternacht.Common.Attributes;
 using Mitternacht.Modules.Utility.Services;
+using Mitternacht.Services.Database;
 
 namespace Mitternacht.Modules.Utility {
 	public partial class Utility {
 		public class CountToNumberCommands : MitternachtSubmodule<CountToNumberService> {
+			private readonly IUnitOfWork _uow;
+
+			public CountToNumberCommands(IUnitOfWork uow) {
+				_uow = uow;
+			}
 
 			[MitternachtCommand, Usage, Description, Aliases]
 			[RequireContext(ContextType.Guild)]
 			[Priority(2)]
 			public async Task CountToNumberChannel() {
-				var channelId = Service.GetCountToNumberChannelId(Context.Guild.Id);
+				var channelId = _uow.GuildConfigs.For(Context.Guild.Id).CountToNumberChannelId;
+
 				if(channelId.HasValue) {
 					var channel = await Context.Guild.GetTextChannelAsync(channelId.Value);
-					if(channel == null)
+
+					if(channel == null) {
 						await ErrorLocalized("counttonumber_channel_not_existing");
-					else
+					} else {
 						await ConfirmLocalized("counttonumber_channel_current", channel.Mention);
+					}
 				} else {
 					await ConfirmLocalized("counttonumber_channel_not_set");
 				}
@@ -29,11 +38,16 @@ namespace Mitternacht.Modules.Utility {
 			[RequireContext(ContextType.Guild)]
 			[Priority(1)]
 			public async Task CountToNumberChannel(ITextChannel channel) {
-				var success = Service.SetCountToNumberChannel(channel.Guild, channel);
-				if(success)
+				var gc = _uow.GuildConfigs.For(Context.Guild.Id);
+
+				if(gc.CountToNumberChannelId != channel.Id) {
+					gc.CountToNumberChannelId = channel.Id;
+					await _uow.SaveChangesAsync(false).ConfigureAwait(false);
+
 					await ConfirmLocalized("counttonumber_channel_set", channel.Mention);
-				else
+				} else {
 					await ErrorLocalized("counttonumber_channel_already_set", channel.Mention);
+				}
 			}
 
 			[MitternachtCommand, Usage, Description, Aliases]
@@ -41,11 +55,16 @@ namespace Mitternacht.Modules.Utility {
 			[Priority(0)]
 			public async Task CountToNumberChannel(string channel) {
 				if(channel.Equals("null", StringComparison.OrdinalIgnoreCase)) {
-					var success = Service.SetCountToNumberChannel(Context.Guild, null);
-					if(success)
+					var gc = _uow.GuildConfigs.For(Context.Guild.Id);
+
+					if(gc.CountToNumberChannelId != null) {
+						gc.CountToNumberChannelId = null;
+						await _uow.SaveChangesAsync(false).ConfigureAwait(false);
+
 						await ConfirmLocalized("counttonumber_channel_removed");
-					else
+					} else {
 						await ErrorLocalized("counttonumber_channel_already_removed");
+					}
 				}
 			}
 
@@ -53,18 +72,24 @@ namespace Mitternacht.Modules.Utility {
 			[MitternachtCommand, Usage, Description, Aliases]
 			[RequireContext(ContextType.Guild)]
 			public async Task CountToNumberMessageChance() {
-				var chance = Service.GetCountToNumberMessageChance(Context.Guild.Id);
+				var chance = _uow.GuildConfigs.For(Context.Guild.Id).CountToNumberMessageChance;
+
 				await ConfirmLocalized("counttonumber_chance_current", chance);
 			}
 
 			[MitternachtCommand, Usage, Description, Aliases]
 			[RequireContext(ContextType.Guild)]
 			public async Task CountToNumberMessageChance(double chance) {
-				var success = Service.SetCountToNumberMessageChance(Context.Guild.Id, chance);
-				if(success)
+				var gc = _uow.GuildConfigs.For(Context.Guild.Id);
+
+				if(gc.CountToNumberMessageChance != chance) {
+					gc.CountToNumberMessageChance = chance;
+					await _uow.SaveChangesAsync(false).ConfigureAwait(false);
+
 					await ConfirmLocalized("counttonumber_chance_set", chance);
-				else
+				} else {
 					await ErrorLocalized("counttonumber_chance_already_set", chance);
+				}
 			}
 		}
 	}
--- src/MitternachtBot/Modules/Utility/Services/CountToNumberService.cs
+++ src/MitternachtBot/Modules/Utility/Services/CountToNumberService.cs
@@ -1,6 +1,4 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using Discord;
@@ -12,77 +10,28 @@ namespace Mitternacht.Modules.Utility.Services {
 		private readonly DiscordSocketClient _client;
 		private readonly DbService _db;
 
-		private Dictionary<ulong, (ulong? ChannelId, double MessageChance)> _countChannelIds;
-
 		private readonly Random _random = new Random();
 
 		public CountToNumberService(DiscordSocketClient client, DbService db) {
 			_client = client;
 			_db = db;
 
-			ReloadGuildConfigs();
 			_client.MessageReceived += CountToMessageReceived;
 		}
 
-		public void ReloadGuildConfigs() {
-			using var uow = _db.UnitOfWork;
-			_countChannelIds = uow.GuildConfigs.GetAll().ToDictionary(gc => gc.GuildId, gc => (gc.CountToNumberChannelId, gc.CountToNumberMessageChance));
-		}
-
-		public bool SetCountToNumberChannel(IGuild guild, ITextChannel channel) {
-			if(_countChannelIds.TryGetValue(guild.Id, out var guildCountToNumberItem) && guildCountToNumberItem.ChannelId == channel?.Id)
-				return false;
-
-			using(var uow = _db.UnitOfWork) {
-				var gc = uow.GuildConfigs.For(guild.Id);
-				gc.CountToNumberChannelId = channel?.Id;
-				uow.GuildConfigs.Update(gc);
-				uow.SaveChanges();
-			}
-			_countChannelIds[guild.Id] = (channel?.Id, _countChannelIds[guild.Id].MessageChance);
-			return true;
-		}
-
-		public ulong? GetCountToNumberChannelId(ulong guildId)
-			=> _countChannelIds.TryGetValue(guildId, out var guildCountToNumberItem) ? guildCountToNumberItem.ChannelId : null;
-
-
-		public bool SetCountToNumberMessageChance(ulong guildId, double chance) {
-			if(_countChannelIds.TryGetValue(guildId, out var guildCountToNumberItem) && Math.Abs(guildCountToNumberItem.MessageChance - chance) < double.Epsilon)
-				return false;
-
-			using(var uow = _db.UnitOfWork) {
-				var gc = uow.GuildConfigs.For(guildId);
-				gc.CountToNumberMessageChance = chance;
-				uow.GuildConfigs.Update(gc);
-				uow.SaveChanges();
-			}
-			_countChannelIds[guildId] = (_countChannelIds[guildId].ChannelId, chance);
-			return true;
-		}
-
-		public double GetCountToNumberMessageChance(ulong guildId)
-			=> _countChannelIds.TryGetValue(guildId, out var guildCountToNumberItem) ? guildCountToNumberItem.MessageChance : 0;
-
-
 		public async Task CountToMessageReceived(SocketMessage msg) {
-			if(!msg.Author.IsBot) {
-				var guildChannels = _countChannelIds.Where(gc => gc.Value.ChannelId != null).Select(gc => (gc.Key, gc.Value.ChannelId.Value, gc.Value.MessageChance)).ToList();
+			if(!msg.Author.IsBot && msg.Channel is ITextChannel channel) {
+				using var uow = _db.UnitOfWork;
+				var gc = uow.GuildConfigs.For(channel.GuildId);
 
-				if(guildChannels.Any(gc => gc.Value == msg.Channel.Id)) {
+				if(gc.CountToNumberChannelId == channel.Id) {
 					var match = Regex.Match(msg.Content.Trim(), "\\A(\\d+)");
 					
 					if(match.Success) {
-						var (guildId, channelId, messageChance) = guildChannels.First(gc => gc.Value == msg.Channel.Id);
-						var guild = _client.GetGuild(guildId);
-						
-						if(guild != null) {
-							var channel = guild.GetTextChannel(channelId);
-							var currentnumber = ulong.Parse(match.Groups[1].Value);
+						var currentnumber = ulong.Parse(match.Groups[1].Value);
 
-							if(_random.NextDouble() < messageChance) {
-								await channel.SendMessageAsync($"{currentnumber + 1}").ConfigureAwait(false);
-							}
+						if(_random.NextDouble() < gc.CountToNumberMessageChance) {
+							await channel.SendMessageAsync($"{currentnumber + 1}").ConfigureAwait(false);
 						}
 					}
 				}
