16aff79855170cd4ad8b160ba6a30f6417c9c14b
expeehaa <expeehaa@outlook.com>
2020-10-04 23:29:11 +0200

    Remove early returns and always use braces for if blocks in "Birthday.cs".

--- src/MitternachtBot/Modules/Birthday/Birthday.cs
+++ src/MitternachtBot/Modules/Birthday/Birthday.cs
@@ -28,34 +28,40 @@ namespace Mitternacht.Modules.Birthday {
 		[RequireContext(ContextType.Guild)]
 		public async Task BirthdaySet(IBirthDate bd) {
 			var bdm = uow.BirthDates.GetUserBirthDate(Context.User.Id);
-			if(!_botCreds.IsOwner(Context.User) && (bdm?.Year != null || bdm != null && bdm.Year == null && (bdm.Day != bd.Day || bdm.Month != bd.Month))) {
-				await ReplyErrorLocalized("birthdayset_set_before").ConfigureAwait(false);
-				return;
-			}
+			
+			if(_botCreds.IsOwner(Context.User) || (bdm?.Year) == null && (bdm == null || bdm.Year != null || bdm.Day == bd.Day && bdm.Month == bd.Month)) {
+				uow.BirthDates.SetBirthDate(Context.User.Id, bd);
+				await uow.SaveChangesAsync(false).ConfigureAwait(false);
 
-			uow.BirthDates.SetBirthDate(Context.User.Id, bd);
-			await uow.SaveChangesAsync(false).ConfigureAwait(false);
+				await ReplyConfirmLocalized("birthdayset_set", bd.ToString()).ConfigureAwait(false);
 
-			await ReplyConfirmLocalized("birthdayset_set", bd.ToString()).ConfigureAwait(false);
+				if(bd.IsBirthday(DateTime.Now) && (bdm == null || !bdm.IsBirthday(DateTime.Now))) {
+					var gc = uow.GuildConfigs.For(Context.Guild.Id);
+					
+					if(gc.BirthdaysEnabled) {
+						var bdmChId = gc.BirthdayMessageChannelId;
+						
+						if(bdmChId != null) {
+							var bdmCh = await Context.Guild.GetTextChannelAsync(bdmChId.Value).ConfigureAwait(false);
+							
+							if(bdmCh != null) {
+								await bdmCh.SendMessageAsync(string.Format(gc.BirthdayMessage, Context.User.Mention)).ConfigureAwait(false);
+							}
+						}
 
-			if(bd.IsBirthday(DateTime.Now) && (bdm == null || !bdm.IsBirthday(DateTime.Now))) {
-				var gc = uow.GuildConfigs.For(Context.Guild.Id);
-				if(!gc.BirthdaysEnabled)
-					return;
-				var bdmChId = gc.BirthdayMessageChannelId;
-				if(bdmChId != null) {
-					var bdmCh = await Context.Guild.GetTextChannelAsync(bdmChId.Value).ConfigureAwait(false);
-					if(bdmCh != null)
-						await bdmCh.SendMessageAsync(string.Format(gc.BirthdayMessage, Context.User.Mention)).ConfigureAwait(false);
-				}
+						var roleId = gc.BirthdayRoleId;
 
-				var roleId = gc.BirthdayRoleId;
-				if(roleId != null) {
-					var role = Context.Guild.GetRole(roleId.Value);
-					if(role != null) {
-						await ((SocketGuildUser)Context.User).AddRoleAsync(role).ConfigureAwait(false);
+						if(roleId != null) {
+							var role = Context.Guild.GetRole(roleId.Value);
+
+							if(role != null) {
+								await ((SocketGuildUser)Context.User).AddRoleAsync(role).ConfigureAwait(false);
+							}
+						}
 					}
 				}
+			} else {
+				await ReplyErrorLocalized("birthdayset_set_before").ConfigureAwait(false);
 			}
 		}
 
@@ -65,20 +71,24 @@ namespace Mitternacht.Modules.Birthday {
 			uow.BirthDates.SetBirthDate(user.Id, bd);
 			await uow.SaveChangesAsync(false).ConfigureAwait(false);
 
-			if(user == Context.User)
+			if(user == Context.User) {
 				await ConfirmLocalized("birthdayset_set", bd.ToString()).ConfigureAwait(false);
-			else
+			} else {
 				await ConfirmLocalized("birthdayset_set_owner", user.ToString(), bd.ToString()).ConfigureAwait(false);
+			}
 		}
 
 		[MitternachtCommand, Usage, Description, Aliases]
 		[OwnerOnly]
 		public async Task BirthdayRemove(IUser user) {
 			var success = uow.BirthDates.DeleteBirthDate(user.Id);
-			if(success)
+			
+			if(success) {
 				await ConfirmLocalized("birthdayremove_removed", user.ToString()).ConfigureAwait(false);
-			else
+			} else {
 				await ErrorLocalized("birthdayremove_remove_failed", user.ToString()).ConfigureAwait(false);
+			}
+
 			await uow.SaveChangesAsync(false).ConfigureAwait(false);
 		}
 
@@ -88,19 +98,22 @@ namespace Mitternacht.Modules.Birthday {
 			user ??= Context.User;
 			var bdm = uow.BirthDates.GetUserBirthDate(user.Id);
 			var age = bdm?.Year != null ? (int)Math.Floor((DateTime.Now - new DateTime(bdm.Year.Value, bdm.Month, bdm.Day)).TotalDays / 365.25) : 0;
-			if(user == Context.User)
-				if(bdm == null)
+			
+			if(user == Context.User) {
+				if(bdm == null) {
 					await ReplyErrorLocalized("birthdayget_self_none").ConfigureAwait(false);
-				else if(bdm.Year == null)
+				} else if(bdm.Year == null) {
 					await ReplyConfirmLocalized("birthdayget_self", bdm.ToString()).ConfigureAwait(false);
-				else
+				} else {
 					await ReplyConfirmLocalized("birthdayget_self_age", bdm.ToString(), age).ConfigureAwait(false);
-			else if(bdm == null)
+				}
+			} else if(bdm == null) {
 				await ReplyErrorLocalized("birthdayget_user_none", user.ToString()).ConfigureAwait(false);
-			else if(bdm.Year == null)
+			} else if(bdm.Year == null) {
 				await ReplyConfirmLocalized("birthdayget_user", user.ToString(), bdm.ToString()).ConfigureAwait(false);
-			else
+			} else {
 				await ReplyConfirmLocalized("birthdayget_user_age", user.ToString(), bdm.ToString(), age).ConfigureAwait(false);
+			}
 		}
 
 		[MitternachtCommand, Usage, Description, Aliases]
@@ -109,9 +122,9 @@ namespace Mitternacht.Modules.Birthday {
 			bd ??= BirthDate.TodayWithoutYear;
 			var birthdates = uow.BirthDates.GetBirthdays(bd, bd.Year.HasValue).ToList();
 
-			if(!birthdates.Any())
+			if(!birthdates.Any()) {
 				await ConfirmLocalized("birthdays_none_date", bd.ToString()).ConfigureAwait(false);
-			else {
+			} else {
 				var eb = new EmbedBuilder()
 					.WithOkColor()
 					.WithTitle(GetText("birthdays_list_title", bd.ToString()))
@@ -127,19 +140,18 @@ namespace Mitternacht.Modules.Birthday {
 				page = 1;
 			var birthdates = uow.BirthDates.GetAll().OrderBy(bdm => bdm.Month).ThenBy(bdm => bdm.Day).ToList();
 
-			if(!birthdates.Any()) {
+			if(birthdates.Any()) {
+				const int elementsPerPage = 10;
+				var pageCount = (int)Math.Ceiling(birthdates.Count * 1d / elementsPerPage);
+				page = page > pageCount ? pageCount : page;
+
+				await Context.Channel.SendPaginatedConfirmAsync(Context.Client as DiscordSocketClient, page - 1,
+					currentPage => new EmbedBuilder().WithOkColor().WithTitle(GetText("birthdaysall_all_title")).WithDescription(string.Join("\n",
+						birthdates.Skip(elementsPerPage * currentPage).Take(elementsPerPage).Select(BdmToString))),
+					pageCount, reactUsers: new[] { Context.User as IGuildUser }).ConfigureAwait(false);
+			} else {
 				await ErrorLocalized("birthdaysall_none").ConfigureAwait(false);
-				return;
 			}
-
-			const int elementsPerPage = 10;
-			var pageCount = (int)Math.Ceiling(birthdates.Count * 1d / elementsPerPage);
-			page = page > pageCount ? pageCount : page;
-
-			await Context.Channel.SendPaginatedConfirmAsync(Context.Client as DiscordSocketClient, page - 1,
-				currentPage => new EmbedBuilder().WithOkColor().WithTitle(GetText("birthdaysall_all_title")).WithDescription(string.Join("\n",
-					birthdates.Skip(elementsPerPage * currentPage).Take(elementsPerPage).Select(BdmToString))),
-				pageCount, reactUsers: new[] { Context.User as IGuildUser }).ConfigureAwait(false);
 		}
 
 		[MitternachtCommand, Usage, Description, Aliases]
@@ -147,12 +159,14 @@ namespace Mitternacht.Modules.Birthday {
 		public async Task BirthdayRole() {
 			var bdayroleid = uow.GuildConfigs.For(Context.Guild.Id).BirthdayRoleId;
 			var bdayrole = bdayroleid.HasValue ? Context.Guild.GetRole(bdayroleid.Value) : null;
-			if(!bdayroleid.HasValue)
+
+			if(!bdayroleid.HasValue) {
 				await ErrorLocalized("birthdayrole_role", GetText("birthdayrole_role_not_set")).ConfigureAwait(false);
-			else if(bdayrole == null)
+			} else if(bdayrole == null) {
 				await ErrorLocalized("birthdayrole_role", GetText("birthdayrole_role_not_existing")).ConfigureAwait(false);
-			else
+			} else {
 				await ConfirmLocalized("birthdayrole_role", bdayrole.Name).ConfigureAwait(false);
+			}
 		}
 
 		[MitternachtCommand, Usage, Description, Aliases]
@@ -187,12 +201,13 @@ namespace Mitternacht.Modules.Birthday {
 		public async Task BirthdayMessageChannel() {
 			var bdayMsgChId = uow.GuildConfigs.For(Context.Guild.Id).BirthdayMessageChannelId;
 			var bdayMsgCh = bdayMsgChId.HasValue ? await Context.Guild.GetTextChannelAsync(bdayMsgChId.Value).ConfigureAwait(false) : null;
-			if(!bdayMsgChId.HasValue)
+			if(!bdayMsgChId.HasValue) {
 				await ErrorLocalized("birthdaymessagechannel_msgch", GetText("birthdaymessagechannel_msgch_not_set")).ConfigureAwait(false);
-			else if(bdayMsgCh == null)
+			} else if(bdayMsgCh == null) {
 				await ErrorLocalized("birthdaymessagechannel_msgch", GetText("birthdaymessagechannel_msgch_not_existing")).ConfigureAwait(false);
-			else
+			} else {
 				await ConfirmLocalized("birthdaymessagechannel_msgch", bdayMsgCh.Name).ConfigureAwait(false);
+			}
 		}
 
 		[MitternachtCommand, Usage, Description, Aliases]
@@ -248,15 +263,15 @@ namespace Mitternacht.Modules.Birthday {
 		[OwnerOnly]
 		public async Task BirthdayReactions(bool enable) {
 			var gc = uow.GuildConfigs.For(Context.Guild.Id);
-			if(gc.BirthdaysEnabled == enable) {
+			
+			if(gc.BirthdaysEnabled != enable) {
+				gc.BirthdaysEnabled = enable;
+				uow.GuildConfigs.Update(gc);
+				await uow.SaveChangesAsync(false).ConfigureAwait(false);
+				await ConfirmLocalized("birthdayreactions_enable", GetText(enable ? "birthdayreactions_enabled" : "birthdayreactions_disabled")).ConfigureAwait(false);
+			} else {
 				await ErrorLocalized("birthdayreactions_enable_same", GetText(enable ? "birthdayreactions_enabled" : "birthdayreactions_disabled")).ConfigureAwait(false);
-				return;
 			}
-
-			gc.BirthdaysEnabled = enable;
-			uow.GuildConfigs.Update(gc);
-			await uow.SaveChangesAsync(false).ConfigureAwait(false);
-			await ConfirmLocalized("birthdayreactions_enable", GetText(enable ? "birthdayreactions_enabled" : "birthdayreactions_disabled")).ConfigureAwait(false);
 		}
 
 		[MitternachtCommand, Usage, Description, Aliases]
@@ -264,16 +279,17 @@ namespace Mitternacht.Modules.Birthday {
 		[OwnerOnly]
 		public async Task BirthdayMoney(long money) {
 			var gc = uow.GuildConfigs.For(Context.Guild.Id);
-			if(gc.BirthdayMoney == money) {
-				await ReplyErrorLocalized("birthdaymoney_money_already_set").ConfigureAwait(false);
-				return;
-			}
-			var oldMoney = gc.BirthdayMoney ?? 0;
-			gc.BirthdayMoney = money;
-			uow.GuildConfigs.Update(gc);
-			await ReplyConfirmLocalized("birthdaymoney_money_set", _botConf.BotConfig.CurrencySign, oldMoney, money).ConfigureAwait(false);
+			
+			if(gc.BirthdayMoney != money) {
+				var oldMoney = gc.BirthdayMoney ?? 0;
+				gc.BirthdayMoney = money;
+				uow.GuildConfigs.Update(gc);
+				await ReplyConfirmLocalized("birthdaymoney_money_set", _botConf.BotConfig.CurrencySign, oldMoney, money).ConfigureAwait(false);
 
-			await uow.SaveChangesAsync(false).ConfigureAwait(false);
+				await uow.SaveChangesAsync(false).ConfigureAwait(false);
+			} else {
+				await ReplyErrorLocalized("birthdaymoney_money_already_set").ConfigureAwait(false);
+			}
 		}
 
 		[MitternachtCommand, Usage, Description, Aliases]
@@ -288,23 +304,22 @@ namespace Mitternacht.Modules.Birthday {
 		[RequireContext(ContextType.Guild)]
 		public async Task BirthdayMessageEvent(bool? enable = null) {
 			var hasBirthdate = uow.BirthDates.HasBirthDate(Context.User.Id);
-			if(!hasBirthdate) {
-				await ReplyErrorLocalized("messageevent_birthday_not_set").ConfigureAwait(false);
-				return;
-			}
-
-			var bdm = uow.BirthDates.GetUserBirthDate(Context.User.Id);
-			if(!enable.HasValue)
-				await ReplyConfirmLocalized($"messageevent_show", GetText(bdm.BirthdayMessageEnabled ? "birthdaymessageevent_enabled" : "birthdaymessageevent_disabled")).ConfigureAwait(false);
-			else {
-				if(bdm.BirthdayMessageEnabled == enable.Value)
-					await ReplyErrorLocalized($"messageevent_already_set", GetText(bdm.BirthdayMessageEnabled ? "birthdaymessageevent_enabled" : "birthdaymessageevent_disabled")).ConfigureAwait(false);
-				else {
-					bdm.BirthdayMessageEnabled = enable.Value;
-					uow.BirthDates.Update(bdm);
-					await uow.SaveChangesAsync(false).ConfigureAwait(false);
-					await ReplyConfirmLocalized($"messageevent_changed", GetText(enable.Value ? "birthdaymessageevent_enabled" : "birthdaymessageevent_disabled")).ConfigureAwait(false);
+			if(hasBirthdate) {
+				var bdm = uow.BirthDates.GetUserBirthDate(Context.User.Id);
+				if(!enable.HasValue) {
+					await ReplyConfirmLocalized($"messageevent_show", GetText(bdm.BirthdayMessageEnabled ? "birthdaymessageevent_enabled" : "birthdaymessageevent_disabled")).ConfigureAwait(false);
+				} else {
+					if(bdm.BirthdayMessageEnabled == enable.Value) {
+						await ReplyErrorLocalized($"messageevent_already_set", GetText(bdm.BirthdayMessageEnabled ? "birthdaymessageevent_enabled" : "birthdaymessageevent_disabled")).ConfigureAwait(false);
+					} else {
+						bdm.BirthdayMessageEnabled = enable.Value;
+						uow.BirthDates.Update(bdm);
+						await uow.SaveChangesAsync(false).ConfigureAwait(false);
+						await ReplyConfirmLocalized($"messageevent_changed", GetText(enable.Value ? "birthdaymessageevent_enabled" : "birthdaymessageevent_disabled")).ConfigureAwait(false);
+					}
 				}
+			} else {
+				await ReplyErrorLocalized("messageevent_birthday_not_set").ConfigureAwait(false);
 			}
 		}
 
