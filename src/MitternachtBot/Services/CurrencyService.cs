79550a35fb3dea77c69b308b575fa9ba6cdf12c9
expeehaa <expeehaa@outlook.com>
2020-10-04 21:39:38 +0200

    Use a more appropriate exception type.

--- src/MitternachtBot/Services/CurrencyService.cs
+++ src/MitternachtBot/Services/CurrencyService.cs
@@ -17,7 +17,7 @@ namespace Mitternacht.Services {
 
 		public async Task<bool> RemoveAsync(ulong guildId, ulong authorId, string reason, long amount, IUnitOfWork uow = null) {
 			if(amount < 0)
-				throw new ArgumentNullException(nameof(amount));
+				throw new ArgumentException(nameof(amount));
 
 			if(uow == null) {
 				using var _uow = _db.UnitOfWork;
@@ -70,7 +70,7 @@ namespace Mitternacht.Services {
 
 		public async Task AddAsync(ulong guildId, ulong receiverId, string reason, long amount, IUnitOfWork uow = null) {
 			if(amount < 0)
-				throw new ArgumentNullException(nameof(amount));
+				throw new ArgumentException(nameof(amount));
 
 			var transaction = new CurrencyTransaction {
 				GuildId = guildId,
