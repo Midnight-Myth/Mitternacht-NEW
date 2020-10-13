88f6f9f6c702171369b6956776a133a99f8bbca9
expeehaa <expeehaa@outlook.com>
2020-10-13 18:40:31 +0200

    Change formatting.

--- src/MitternachtBot/Common/TypeReaders/BotCommandTypeReader.cs
+++ src/MitternachtBot/Common/TypeReaders/BotCommandTypeReader.cs
@@ -11,20 +11,11 @@ namespace Mitternacht.Common.TypeReaders {
 		public override Task<TypeReaderResult> ReadAsync(ICommandContext context, string input, IServiceProvider services) {
 			var cmds       = services.GetService<CommandService>();
 			var cmdHandler = services.GetService<CommandHandler>();
+			var prefix     = cmdHandler.GetPrefix(context.Guild);
+			var cmd        = (input.StartsWith(prefix, StringComparison.OrdinalIgnoreCase) ? cmds.Commands.FirstOrDefault(c => c.Aliases.Any(ca => ca.Equals(input[prefix.Length..], StringComparison.OrdinalIgnoreCase))) : null)
+				?? cmds.Commands.FirstOrDefault(c => c.Aliases.Any(ca => ca.Equals(input, StringComparison.OrdinalIgnoreCase)));
 
-			var prefix = cmdHandler.GetPrefix(context.Guild);
-
-			CommandInfo cmd = null;
-			if(input.StartsWith(prefix, StringComparison.OrdinalIgnoreCase)) {
-				var inputWithoutPrefix = input.Substring(prefix.Length);
-
-				cmd = cmds.Commands.FirstOrDefault(c => c.Aliases.Any(ca => ca.Equals(inputWithoutPrefix, StringComparison.OrdinalIgnoreCase)));
-			}
-			if(cmd == null) {
-				cmd = cmds.Commands.FirstOrDefault(c => c.Aliases.Any(ca => ca.Equals(input, StringComparison.OrdinalIgnoreCase)));
-			}
-
-			return Task.FromResult(cmd == null ? TypeReaderResult.FromError(CommandError.ParseFailed, "No such command found.") : TypeReaderResult.FromSuccess(cmd));
+			return Task.FromResult(cmd == null ? TypeReaderResult.FromError(CommandError.ParseFailed, $"No command named '{input}' found.") : TypeReaderResult.FromSuccess(cmd));
 		}
 	}
 
@@ -32,16 +23,17 @@ namespace Mitternacht.Common.TypeReaders {
 		public override async Task<TypeReaderResult> ReadAsync(ICommandContext context, string input, IServiceProvider services) {
 			var crs = services.GetService<CustomReactionsService>();
 
-			if(!crs.GlobalReactions.Any(x => x.Trigger.Equals(input, StringComparison.OrdinalIgnoreCase))) {
+			if(crs.GlobalReactions.Any(x => x.Trigger.Equals(input, StringComparison.OrdinalIgnoreCase))) {
+				return TypeReaderResult.FromSuccess(new CommandOrCrInfo(input));
+			} else {
 				var guild = context.Guild;
+				
 				if(guild != null && crs.ReactionsForGuild(guild.Id).Concat(crs.GlobalReactions).Any(x => x.Trigger.Equals(input, StringComparison.OrdinalIgnoreCase))) {
 					return TypeReaderResult.FromSuccess(new CommandOrCrInfo(input));
 				} else {
 					var cmd = await base.ReadAsync(context, input, services);
 					return cmd.IsSuccess ? TypeReaderResult.FromSuccess(new CommandOrCrInfo(((CommandInfo)cmd.Values.First().Value).Name)) : TypeReaderResult.FromError(CommandError.ParseFailed, "No such command or cr found.");
 				}
-			} else {
-				return TypeReaderResult.FromSuccess(new CommandOrCrInfo(input));
 			}
 		}
 	}
