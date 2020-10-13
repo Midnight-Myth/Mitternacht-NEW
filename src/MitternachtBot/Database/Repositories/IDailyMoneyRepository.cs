42a98482c8b420499c1f1a4b9d8aa2afc821f09c
expeehaa <expeehaa@outlook.com>
2020-10-13 02:12:41 +0200

    Hopefully make dailmoney receiving dependent on the guilds time zone.

--- src/MitternachtBot/Database/Repositories/IDailyMoneyRepository.cs
+++ src/MitternachtBot/Database/Repositories/IDailyMoneyRepository.cs
@@ -5,8 +5,8 @@ namespace Mitternacht.Database.Repositories {
 	public interface IDailyMoneyRepository : IRepository<DailyMoney> {
 		DailyMoney GetOrCreate(ulong guildId, ulong userId);
 		DateTime GetLastReceived(ulong guildId, ulong userId);
-		bool CanReceive(ulong guildId, ulong userId);
+		bool CanReceive(ulong guildId, ulong userId, TimeZoneInfo timeZoneInfo);
 		DateTime UpdateState(ulong guildId, ulong userId);
-		void ResetLastTimeReceived(ulong guildId, ulong userId);
+		void ResetLastTimeReceived(ulong guildId, ulong userId, TimeZoneInfo timeZoneInfo);
 	}
 }
