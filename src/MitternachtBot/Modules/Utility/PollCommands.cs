bb3b86dea0d112efee41b2d739bcf4cd9cb6f718
expeehaa <expeehaa@outlook.com>
2021-02-02 17:12:57 +0100

    Add simple reaction-based poll command.

--- src/MitternachtBot/Modules/Utility/PollCommands.cs
+++ src/MitternachtBot/Modules/Utility/PollCommands.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Linq;
 using System.Threading.Tasks;
 using Discord;
 using Discord.Commands;
@@ -35,6 +37,30 @@ namespace Mitternacht.Modules.Utility {
 			public async Task PollEnd() {
 				Service.ActivePolls.TryRemove(Context.Guild.Id, out var poll);
 				await poll.StopPoll().ConfigureAwait(false);
+			}
+
+			[MitternachtCommand, Usage, Description, Aliases]
+			[RequireUserPermission(GuildPermission.ManageMessages)]
+			[RequireContext(ContextType.Guild)]
+			public async Task PollReaction(string question, params string[] answers){
+				if(answers.Length <= 26){
+					var eb = new EmbedBuilder().WithOkColor().WithTitle(GetText("pollreaction_title", question));
+
+					var emojiAnswers = answers.Select((s, i) => (emoji: IndexToRegionalIndicator(i), answer: s)).ToArray();
+
+					eb.WithDescription(emojiAnswers.Aggregate("", (s, answer) => $"{s}\n{GetText("pollreaction_answer", answer.emoji, answer.answer)}", s => s.Trim()));
+
+					var msg = await Context.Channel.EmbedAsync(eb).ConfigureAwait(false);
+
+					_ = Task.Run(async () => await msg.AddReactionsAsync(emojiAnswers.Select(a => a.emoji).ToArray()).ConfigureAwait(false));
+				}
+			}
+
+			private static Emoji IndexToRegionalIndicator(int i){
+				return i switch {
+					>= 0 and <= 26 => new Emoji(char.ConvertFromUtf32(0x1F1E6 + i)),
+					_ => throw new ArgumentOutOfRangeException(nameof(i)),
+				};
 			}
 		}
 	}
