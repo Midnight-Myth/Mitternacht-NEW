044e022ddec05ef5120ed3e6c4c36eb773ede05d
expeehaa <expeehaa@outlook.com>
2021-06-09 18:58:17 +0200

    Rename fields in UserHelper and use target-typed new.

--- MitternachtWeb/Helpers/UserHelper.cs
+++ MitternachtWeb/Helpers/UserHelper.cs
@@ -16,15 +16,15 @@ namespace MitternachtWeb.Helpers {
 		public const double DiscordUserCacheTime = 60.0;
 		public const double DiscordUserGuildsCacheTime = 300.0;
 
-		private static readonly Dictionary<ulong, (DateTime RequestTime, DiscordUser User)> discordUsers = new Dictionary<ulong, (DateTime, DiscordUser)>();
-		private static readonly Dictionary<ulong, (DateTime RequestTime, ulong[] Guilds  )> userGuilds   = new Dictionary<ulong, (DateTime, ulong[]    )>();
+		private static readonly Dictionary<ulong, (DateTime RequestTime, DiscordUser User)> DiscordUsers = new();
+		private static readonly Dictionary<ulong, (DateTime RequestTime, ulong[] Guilds  )> UserGuilds   = new();
 
-		private static readonly HttpClient httpClient = new HttpClient();
+		private static readonly HttpClient HttpClient = new();
 
 		public static async Task<DiscordUser> GetDiscordUserAsync(ClaimsPrincipal user, HttpContext context) {
 			var userIdString = user.Claims.FirstOrDefault(c => c.Type == ClaimTypes.NameIdentifier)?.Value;
 			if(ulong.TryParse(userIdString, out var userId)) {
-				var success = discordUsers.TryGetValue(userId, out var t);
+				var success = DiscordUsers.TryGetValue(userId, out var t);
 
 				if(success && (t.RequestTime - DateTime.UtcNow).TotalSeconds < DiscordUserCacheTime) {
 					return t.User;
@@ -39,7 +39,7 @@ namespace MitternachtWeb.Helpers {
 						BotPagePermissions   = botPagePerms,
 						GuildPagePermissions = guildPagePerms ?? new Dictionary<ulong, GuildLevelPermission>()
 					};
-					discordUsers.Add(userId, (DateTime.UtcNow, discordUser));
+					DiscordUsers.Add(userId, (DateTime.UtcNow, discordUser));
 					return discordUser;
 				}
 			} else {
@@ -51,7 +51,7 @@ namespace MitternachtWeb.Helpers {
 			var userIdString = user.Claims.FirstOrDefault(c => c.Type == ClaimTypes.NameIdentifier)?.Value;
 
 			if(ulong.TryParse(userIdString, out var userId)) {
-				var success = userGuilds.TryGetValue(userId, out var t);
+				var success = UserGuilds.TryGetValue(userId, out var t);
 
 				if(success && (t.RequestTime - DateTime.UtcNow).TotalSeconds < DiscordUserGuildsCacheTime) {
 					return t.Guilds;
@@ -60,14 +60,14 @@ namespace MitternachtWeb.Helpers {
 					request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", await context.GetTokenAsync("access_token"));
 					request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
 
-					var response = await httpClient.SendAsync(request);
+					var response = await HttpClient.SendAsync(request);
 
 					if(response.IsSuccessStatusCode) {
 						var content = JsonConvert.DeserializeObject<JArray>(await response.Content.ReadAsStringAsync());
 						var botGuilds = Program.MitternachtBot.Client.Guilds.Select(g => g.Id).ToArray();
 						var guilds = content.Select(o => o.Value<ulong>("id")).Intersect(botGuilds).Distinct().ToArray();
 
-						userGuilds.Add(userId, (DateTime.UtcNow, guilds));
+						UserGuilds.Add(userId, (DateTime.UtcNow, guilds));
 						return guilds;
 					} else {
 						return null;
@@ -79,8 +79,8 @@ namespace MitternachtWeb.Helpers {
 		}
 
 		public static void RemoveCache(ulong userId) {
-			discordUsers.Remove(userId);
-			userGuilds.Remove(userId);
+			DiscordUsers.Remove(userId);
+			UserGuilds.Remove(userId);
 		}
 	}
 }
