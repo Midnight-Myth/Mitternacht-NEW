f903faba1dae9177c09fb6f1938e4137031673b1
expeehaa <expeehaa@outlook.com>
2026-05-04 02:05:03 +0200

    Run "dotnet ef migrations add update_to_ef_core_10".

--- src/MitternachtBot/Migrations/Mitternacht/20260504000215_update_to_ef_core_10.Designer.cs
+++ src/MitternachtBot/Migrations/Mitternacht/20260504000215_update_to_ef_core_10.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Mitternacht.Database;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace Mitternacht.Migrations.Mitternacht
 {
     [DbContext(typeof(MitternachtContext))]
-    partial class MitternachtContextModelSnapshot : ModelSnapshot
+    [Migration("20260504000215_update_to_ef_core_10")]
+    partial class update_to_ef_core_10
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/MitternachtBot/Migrations/Mitternacht/20260504000215_update_to_ef_core_10.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Mitternacht.Migrations.Mitternacht
+{
+    /// <inheritdoc />
+    public partial class update_to_ef_core_10 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Discriminator",
+                table: "UsernameHistory",
+                type: "character varying(21)",
+                maxLength: 21,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Discriminator",
+                table: "UsernameHistory",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(21)",
+                oldMaxLength: 21);
+        }
+    }
+}
